8053b581c892e3ec526575b99bec2de842980468
Ilya Daraseliya <idarase@gmail.com>
2022-11-28 07:28:36 +0100

    logo font

--- components/AppLayout/AppLayout.tsx
+++ components/AppLayout/AppLayout.tsx
@@ -3,13 +3,22 @@ import {PropsWithChildren} from 'react';
 import {Card} from '../DateCard/Card/Card';
 import {Text} from '../Text/Text';
 import {UserSecsion} from '../UserSecsion/UserSecsion';
+import {Nerko_One} from '@next/font/google';
+import clsx from 'clsx';
+
+const logoFong = Nerko_One({
+  weight: '400',
+  display: 'swap',
+});
 
 export default function AppLayout({children}: PropsWithChildren) {
   return (
     <div className=" p-4 max-w-xl m-auto">
       <Card className="flex mb-4">
         <Link href="/dashboard" className="mr-2">
-          <Text>WannaGo</Text>
+          <span className={clsx(logoFong.className, 'text-2xl leading-none')}>
+            WannaGo
+          </span>
         </Link>
         <div className="grow" />
         <UserSecsion />
