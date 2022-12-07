d2485514f54ee24ac92f7396bae996a703140c1e
Ilya Daraseliya <idarase@gmail.com>
2022-12-07 13:12:33 +0100

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,13 @@ import {trpc} from '../utils/trpc';
 import {Toaster} from 'react-hot-toast';
 import {bodyFont} from '../fonts';
 import clsx from 'clsx';
+import {getBaseUrl} from '../utils/getBaseUrl';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href={`${getBaseUrl()}/api/favicon`} />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
---
+++ pages/api/favicon.tsx
@@ -0,0 +1,40 @@
+import {ImageResponse} from '@vercel/og';
+import {NextRequest} from 'next/server';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+const fontResponse = fetch(
+  new URL('../../public/PaytoneOne-Regular.ttf', import.meta.url)
+).then(res => res.arrayBuffer());
+
+export default async function handler(req: NextRequest) {
+  const [font] = await Promise.all([fontResponse]);
+
+  return new ImageResponse(
+    (
+      <div tw="flex flex-col justify-center items-center bg-gray-800 text-gray-100 w-full h-full">
+        <span
+          tw="text-2xl leading-none text-center"
+          style={{
+            fontFamily: 'Font',
+          }}
+        >
+          GO
+        </span>
+      </div>
+    ),
+    {
+      width: 50,
+      height: 50,
+      fonts: [
+        {
+          name: 'Font',
+          data: font,
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
