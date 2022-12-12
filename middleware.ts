587f5a938d8e122d036297855b6bb02ed1ef908d
Ilya Daraseliya <idarase@gmail.com>
2022-12-12 20:22:27 +0100

    clean up middleware

--- middleware.ts
+++ middleware.ts
@@ -4,45 +4,22 @@ import type {NextRequest} from 'next/server';
 import {NextURL} from 'next/dist/server/web/next-url';
 import {getBaseUrl} from './utils/getBaseUrl';
 
-type VercelEnv = 'development' | 'preview' | 'production';
-
-const loginRedirectUrl: Record<VercelEnv, string> = {
-  production: 'https://accounts.wannago.app/sign-in',
-  preview: `${getBaseUrl()}/login`,
-  development: `${getBaseUrl()}/login`,
-};
+const publicPathnames = [
+  '/e/',
+  '/images/',
+  '/api/trpc/event.getEventByNanoId',
+  '/register',
+  '/login',
+  '/api',
+];
 
 const isProtectedRoute = (nextUrl: NextURL): boolean => {
   const isHomePage = nextUrl.pathname === '/';
-  const isPublicEventPage = nextUrl.pathname.startsWith('/e/');
-  const isStaticImagePath = nextUrl.pathname.startsWith('/images/');
-  const isGetEventByNanoId = nextUrl.pathname.startsWith(
-    '/api/trpc/event.getEventByNanoId'
+  const isPublicPath = publicPathnames.some(path =>
+    nextUrl.pathname.startsWith(path)
   );
-  const isRegistrationPage = nextUrl.pathname.startsWith('/register');
-  const isApiRoute = nextUrl.pathname.startsWith('/api');
 
-  if (isApiRoute) {
-    return false;
-  }
-
-  if (isStaticImagePath) {
-    return false;
-  }
-
-  if (isRegistrationPage) {
-    return false;
-  }
-
-  if (isHomePage) {
-    return false;
-  }
-
-  if (isPublicEventPage) {
-    return false;
-  }
-
-  if (isGetEventByNanoId) {
+  if (isPublicPath || isHomePage) {
     return false;
   }
 
@@ -51,36 +28,14 @@ const isProtectedRoute = (nextUrl: NextURL): boolean => {
 
 export default withClerkMiddleware((request: NextRequest) => {
   const auth = getAuth(request);
-  const requestHeaders = new Headers(request.headers);
-  // const userId = auth?.userId || requestHeaders.get('x-user-id');
-  const isLoginPage = request.nextUrl.pathname === '/login';
 
-  if (!auth.userId && !isLoginPage && isProtectedRoute(request.nextUrl)) {
-    const redirectUrl = loginRedirectUrl[process.env.VERCEL_ENV as VercelEnv];
-    return NextResponse.redirect(redirectUrl);
+  if (!auth.userId && isProtectedRoute(request.nextUrl)) {
+    return NextResponse.redirect(`${getBaseUrl()}/login`);
   }
-
-  // if (userId) {
-  //   requestHeaders.set('x-user-id', userId);
-  // }
-
-  // return NextResponse.next({
-  //   request: {
-  //     headers: requestHeaders,
-  //   },
-  // });
 
   return NextResponse.next();
 });
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next
-     * - static (static files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!static|_next|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!static|_next|_next/image|favicon.ico).*)'],
 };
