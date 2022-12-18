8d4f18b647cdd906e279e67f5ad986aba11928c9
Ilya Daraseliya <idarase@gmail.com>
2022-12-18 08:33:26 +0100

    update preview image

--- apps/web/pages/api/og-image.tsx
+++ apps/web/pages/api/og-image.tsx
@@ -13,8 +13,8 @@ export default async function handler(req: NextRequest) {
     fetch(new URL('../../public/paytone-one.ttf', import.meta.url)).then(res =>
       res.arrayBuffer()
     ),
-    fetch(new URL('../../public/inter-bold.ttf', import.meta.url)).then(res =>
-      res.arrayBuffer()
+    fetch(new URL('../../public/dm-serif-display.ttf', import.meta.url)).then(
+      res => res.arrayBuffer()
     ),
   ]);
 
@@ -31,29 +31,39 @@ export default async function handler(req: NextRequest) {
   return new ImageResponse(
     (
       <div
-        tw="flex w-full h-full flex-col"
-        style={{
-          backgroundImage: `url(${event?.featuredImageSrc!})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-        }}
+        tw="flex w-full h-full flex-col p-8"
+        style={{backgroundColor: '#FFE5D9'}}
       >
-        <div tw="flex flex-col px-8 py-4 mt-8 ml-8 bg-white rounded-xl leading-none">
-          <span
-            style={{
-              fontSize: 60,
-              marginBottom: 4,
-              fontFamily: 'Body',
-            }}
-          >{`${event?.title}`}</span>
-        </div>
         <div
+          tw="flex w-full h-full border-4 border-gray-700 flex-col p-8 bg-gray-50"
+          style={{borderRadius: 50}}
+        >
+          <div
+            tw="flex grow w-full mb-6"
+            style={{
+              backgroundImage: `url(${event?.featuredImageSrc!})`,
+              backgroundPosition: 'center center',
+              backgroundSize: 'cover',
+              borderRadius: 50,
+            }}
+          />
+          <div tw="flex flex-col rounded-xl leading-none text-slate-800">
+            <span
+              style={{
+                fontSize: 80,
+                marginBottom: 4,
+                fontFamily: 'Body',
+              }}
+            >{`${event?.title}`}</span>
+          </div>
+        </div>
+        {/* <div
           tw="flex absolute right-5 bottom-5 flex-col bg-gray-800 rounded-xl py-4 px-6 text-slate-100 uppercase leading-none text-left text-6xl"
           style={{fontFamily: 'Logo'}}
         >
           <div>wanna</div>
           <div>go</div>
-        </div>
+        </div> */}
       </div>
     ),
     {
