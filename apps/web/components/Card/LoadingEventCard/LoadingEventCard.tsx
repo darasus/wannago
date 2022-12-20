823c702432c2c32b4877e7ef828741caae5064fe
Ilya Daraseliya <idarase@gmail.com>
2022-12-20 07:47:27 +0100

    loading card update

--- apps/web/components/Card/LoadingEventCard/LoadingEventCard.tsx
+++ apps/web/components/Card/LoadingEventCard/LoadingEventCard.tsx
@@ -3,10 +3,10 @@ import {CardBase} from '../CardBase/CardBase';
 export function LoadingEventCard() {
   return (
     <CardBase className="animate-pulse flex flex-col">
-      <div className="grow overflow-hidden relative justify-center bg-slate-500 rounded-3xl aspect-video mb-4" />
+      <div className="grow overflow-hidden relative justify-center bg-gray-200 rounded-3xl aspect-video mb-4" />
       <div>
-        <div className="h-3 bg-slate-500 rounded col-span-2 mb-2" />
-        <div className="h-3 bg-slate-500 rounded col-span-2" />
+        <div className="h-3 bg-gray-200 rounded col-span-2 mb-2" />
+        <div className="h-3 bg-gray-200 rounded col-span-2" />
       </div>
     </CardBase>
   );
