1fe8b4b8b7544eb940b3130636aa7895a90d2df5
Ilya Daraseliya <idarase@gmail.com>
2022-12-20 22:16:13 +0100

    fix test

--- apps/web-e2e/models/Auth.ts
+++ apps/web-e2e/models/Auth.ts
@@ -23,6 +23,8 @@ export class Auth {
     await this.page.getByTestId('login-button').click();
     await this.page.type('#identifier-field', 'idarase+clerk_test@gmail.com');
     await this.page.click('.cl-formButtonPrimary');
-    await this.page.type('[name="codeInput-0"]', '424242');
+    await this.page.type('[name="codeInput-0"]', '424242', {delay: 100});
+
+    await this.page.waitForTimeout(3000);
   }
 }
--- apps/web-e2e/tests/login.spec.ts
+++ apps/web-e2e/tests/login.spec.ts
@@ -4,9 +4,10 @@ import {Auth} from '../models/Auth';
 test('Can login', async ({page}) => {
   const auth = new Auth(page);
   await auth.login();
-  const el = await page.getByTestId('add-event-button');
 
-  await page.reload();
+  await page.goto('http://localhost:3000/dashboard');
+
+  const el = await page.getByTestId('add-event-button');
 
   await expect(el).toBeVisible();
 });
