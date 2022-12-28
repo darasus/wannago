afe73bb09086efc33e91c5f8008420eae8942f24
Ilya Daraseliya <idarase@gmail.com>
2022-12-28 14:40:30 +0100

    add default date to add event form

--- apps/web/components/EventForm/hooks/useEventForm.ts
+++ apps/web/components/EventForm/hooks/useEventForm.ts
@@ -3,17 +3,21 @@ import {useForm} from 'react-hook-form';
 import {formatDate} from '../../../utils/formatDate';
 import {Form} from '../types';
 
+const formatDateForInput = (date: Date | string) => {
+  return formatDate(new Date(date), "yyyy-MM-dd'T'HH:mm");
+};
+
 export function useEventForm(props?: {event?: Event}) {
   const {event} = props || {};
   const form = useForm<Form>({
     defaultValues: {
       title: event?.title,
       startDate: event?.startDate
-        ? formatDate(new Date(event?.startDate), "yyyy-MM-dd'T'HH:mm")
-        : undefined,
+        ? formatDateForInput(event?.startDate)
+        : formatDateForInput(new Date()),
       endDate: event?.endDate
-        ? formatDate(new Date(event?.endDate), "yyyy-MM-dd'T'HH:mm")
-        : undefined,
+        ? formatDateForInput(event?.endDate)
+        : formatDateForInput(new Date()),
       description: event?.description,
       address: event?.address,
       maxNumberOfAttendees: event?.maxNumberOfAttendees || undefined,
