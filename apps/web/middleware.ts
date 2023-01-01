89ad96e114441b94907c2740354be2e51f2e8c16
Ilya Daraseliya <idarase@gmail.com>
2023-01-01 07:11:44 +0100

    set caching in response middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,17 +1,19 @@
 import {withClerkMiddleware} from '@clerk/nextjs/server';
 import {NextRequest, NextResponse} from 'next/server';
+import {ONE_WEEK_IN_SECONDS} from './constants';
 
 export default withClerkMiddleware((req: NextRequest) => {
   console.log(req.nextUrl.pathname);
 
   if (req.nextUrl.pathname === '/api/trpc/event.getRandomExample') {
-    const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
+    const response = NextResponse.next();
 
-    return NextResponse.next({
-      headers: {
-        'Cache-Control': `s-maxage=10, stale-while-revalidate=${ONE_WEEK_IN_SECONDS}`,
-      },
-    });
+    response.headers.set(
+      'Cache-Control',
+      `s-maxage=10, stale-while-revalidate=${ONE_WEEK_IN_SECONDS}`
+    );
+
+    return response;
   }
 
   return NextResponse.next();
