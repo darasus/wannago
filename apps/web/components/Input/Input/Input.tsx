bed1c8f86ad21f02d31ba38ebee14c45f52edb72
Ilya Daraseliya <idarase@gmail.com>
2023-01-04 08:05:18 +0100

    give input same height as button

--- apps/web/components/Input/Input/Input.tsx
+++ apps/web/components/Input/Input/Input.tsx
@@ -29,7 +29,7 @@ export const Input = forwardRef(function Input(
         data-tooltip-target="tooltip-default"
         type="text"
         className={clsx(
-          'block w-full rounded-full border-2 border-gray-300 sm:text-md',
+          'h-11 block w-full rounded-full border-2 border-gray-300 sm:text-md',
           {'focus:border-gray-500 focus:ring-gray-500': !hasError},
           inputClassName,
           {
