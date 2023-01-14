9441dbc55cceadca3cbf806ca854f992d262b97f
Ilya Daraseliya <idarase@gmail.com>
2023-01-14 18:37:02 +0100

    update features copy

--- apps/web/components/Marketing/Features.tsx
+++ apps/web/components/Marketing/Features.tsx
@@ -7,6 +7,7 @@ import {
   BuildingStorefrontIcon,
   ChatBubbleLeftEllipsisIcon,
   EnvelopeOpenIcon,
+  EllipsisHorizontalIcon,
 } from '@heroicons/react/24/outline';
 import {SectionContainer} from './SectionContainer';
 import {titleFontClassName} from '../../fonts';
@@ -16,36 +17,43 @@ const features = [
     name: 'Events',
     summary: 'Create event page with ease',
     description:
-      'With WannaGo, you can quickly and easily create a shareable event page and customize it with all the details your guests need to know.',
+      'With WannaGo, you can quickly and easily create a shareable event page with all the details your guests need to know.',
     icon: <CalendarDaysIcon width={25} height={25} />,
   },
   {
     name: 'Guests',
     summary: 'Invite only the guests you want',
     description:
-      "Our platform allows you to invite only the people you want to attend your event, ensuring that it stays private, whether it's your friends, family or twitter following.",
+      "Whether it's your friends, family, coworkers or Twitter followers we let you choose who you want to invite.",
     icon: <UserGroupIcon width={25} height={25} />,
   },
   {
-    name: 'Branding',
-    summary: 'Customizable event pages',
+    name: 'Looks',
+    summary: 'Beautiful event page',
     description:
-      'You can upload photo, add details about the date, time, and location of your event, and even include information about the dress code or any special instructions.',
+      'You can upload photo, add relevant details with time and location or dress code and any special instructions all wrapped in a beautiful design.',
     icon: <BuildingStorefrontIcon width={25} height={25} />,
   },
   {
     name: 'Comms',
-    summary: 'Easy communication with guests',
+    summary: 'Two-way chat',
     description:
-      "Send reminders and messages to your guests to make sure they don't miss out on your event.",
+      "Send reminders, messages or announcements to your guests to make sure they don't miss out or let them ask questions.",
     icon: <ChatBubbleLeftEllipsisIcon width={25} height={25} />,
   },
   {
     name: 'Invites',
-    summary: 'RSVP tracking',
+    summary: 'Simple RSVPs',
     description:
       'Keep track of who is attending your event with our easy-to-use RSVP system. You can see at a glance who has confirmed their attendance and who is still on the fence.',
     icon: <EnvelopeOpenIcon width={25} height={25} />,
+  },
+  {
+    name: 'Other',
+    summary: 'And much more...',
+    description:
+      'Explore WannaGo and let us know how we can improve your experience with us.',
+    icon: <EllipsisHorizontalIcon width={25} height={25} />,
   },
 ];
 
@@ -82,9 +90,8 @@ export function Features() {
     <SectionContainer id="features" className="bg-white">
       <Container className="my-0">
         <SectionHeader
-          title="Easiest way to invite your network to your place"
-          description="All you need to do is welcome your guests and we will take care of
-            the rest."
+          title="Easiest way to invite"
+          description="We provide the tools you need to invite your network while you focus on what's important"
         />
         <div className="grid lg:grid-cols-12 gap-10">
           {features.map(feature => (
