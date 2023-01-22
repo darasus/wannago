ae76386e19a1d4e2a16a782ac3ddb3b42a4d53da
Ilya Daraseliya <idarase@gmail.com>
2023-01-22 08:52:21 +0100

    fix table styles

--- apps/web/components/Table/Table.tsx
+++ apps/web/components/Table/Table.tsx
@@ -7,8 +7,8 @@ export function Table({children}: Props) {
   return (
     <CardBase>
       <div className="flex flex-col">
-        <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+        <div className="overflow-x-auto">
+          <div className="inline-block min-w-full align-middle">
             <div className="overflow-hidden shadow-none">
               <table className="min-w-full divide-y-2 divide-gray-800">
                 {children}
