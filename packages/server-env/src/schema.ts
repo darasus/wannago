9689c819965056b51f1beffa7f51e054276dc3fb
Ilya Daraseliya <idarase@gmail.com>
2023-01-24 20:05:56 +0100

    remove mailgun

--- packages/server-env/src/schema.ts
+++ packages/server-env/src/schema.ts
@@ -6,7 +6,6 @@ export const serverSchema = z.object({
   UPSTASH_REDIS_REST_TOKEN: z.string(),
   CLOUDFLARE_API_KEY: z.string(),
   QSTASH_TOKEN: z.string(),
-  MAILGUN_API_KEY: z.string(),
   POSTMARK_API_KEY: z.string(),
   TELEGRAM_CHAT_BOT_TOKEN: z.string(),
   TELEGRAM_CHANNEL_ID: z.string(),
