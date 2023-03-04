75b91c8786e0a2cfb02fd5c2f2adeb0f8e2e0b9a
Ilya Daraseliya <idarase@gmail.com>
2023-03-04 21:09:05 +0100

    fix loading animation on card

--- packages/cards/src/LoadingEventCard/LoadingEventCard.tsx
+++ packages/cards/src/LoadingEventCard/LoadingEventCard.tsx
@@ -2,9 +2,9 @@ import {CardBase} from 'ui';
 
 export function LoadingEventCard() {
   return (
-    <CardBase className="animate-pulse flex flex-col">
-      <div className="grow overflow-hidden relative justify-center bg-gray-200 rounded-3xl aspect-video mb-4" />
-      <div>
+    <CardBase className="flex flex-col">
+      <div className="grow overflow-hidden relative justify-center bg-gray-200 rounded-3xl aspect-video mb-4 animate-pulse" />
+      <div className="animate-pulse">
         <div className="h-3 bg-gray-200 rounded col-span-2 mb-2" />
         <div className="h-3 bg-gray-200 rounded col-span-2" />
       </div>
