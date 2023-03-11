d0d952dd6450c67ab7e6e0b864f1a5ae422f403d
Ilya Daraseliya <idarase@gmail.com>
2023-03-11 16:11:53 +0100

    fix base url

--- packages/utils/src/getBaseUrl.ts
+++ packages/utils/src/getBaseUrl.ts
@@ -1,5 +1,11 @@
 import {env} from 'client-env';
 
 export const getBaseUrl = () => {
-  return env.NEXT_PUBLIC_BASE_URL;
+  // return env.NEXT_PUBLIC_BASE_URL;
+
+  if (process.env.NODE_ENV === 'development') {
+    return `http://localhost:3000`;
+  }
+
+  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 };
