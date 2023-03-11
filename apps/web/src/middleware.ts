edbfaa0dfe7921a48735a9244d3485e9d5406a82
Ilya Daraseliya <idarase@gmail.com>
2023-03-11 10:08:14 +0100

    revert middleware

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,36 +1,8 @@
 import {withClerkMiddleware} from '@clerk/nextjs/server';
 import {NextResponse} from 'next/server';
 
-export default withClerkMiddleware((req, res) => {
-  const url = new URL(req.url);
-
-  // if (url.pathname === '/api/edge-flags') {
-  //   if (typeof req.geo?.city !== 'undefined') {
-  //     url.searchParams.set('city', req.geo.city);
-  //   }
-
-  //   if (typeof req.geo?.country !== 'undefined') {
-  //     url.searchParams.set('country', req.geo?.country);
-  //   }
-
-  //   if (typeof req.geo?.region !== 'undefined') {
-  //     url.searchParams.set('region', req.geo.region);
-  //   }
-
-  //   if (typeof req.geo?.latitude !== 'undefined') {
-  //     url.searchParams.set('latitude', req.geo.latitude);
-  //   }
-
-  //   if (typeof req.geo?.longitude !== 'undefined') {
-  //     url.searchParams.set('longitude', req.geo.longitude);
-  //   }
-
-  //   if (typeof req.ip !== 'undefined') {
-  //     url.searchParams.set('ip', req.ip);
-  //   }
-  // }
-
-  return NextResponse.rewrite(url);
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
 });
 
 export const config = {
