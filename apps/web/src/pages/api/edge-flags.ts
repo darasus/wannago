62c4af6c72c4e26eb5de8512633a72ceb57b5926
Ilya Daraseliya <idarase@gmail.com>
2023-03-11 08:30:49 +0100

    disable feature cache

--- apps/web/src/pages/api/edge-flags.ts
+++ apps/web/src/pages/api/edge-flags.ts
@@ -2,7 +2,7 @@ import {createEdgeHandler} from '@upstash/edge-flags';
 import {env} from 'server-env';
 
 export default createEdgeHandler({
-  maxAge: 60,
+  maxAge: 0,
   redisUrl: env.UPSTASH_REDIS_REST_URL,
   redisToken: env.UPSTASH_REDIS_REST_TOKEN,
   environment: env.VERCEL_ENV,
