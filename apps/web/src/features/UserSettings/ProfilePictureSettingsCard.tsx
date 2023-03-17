949a3e259435e730a22115bd24ed33bb2f6bbd0f
Ilya Daraseliya <idarase@gmail.com>
2023-03-17 17:54:13 +0100

    fix layout file upload in settings

--- apps/web/src/features/UserSettings/ProfilePictureSettingsCard.tsx
+++ apps/web/src/features/UserSettings/ProfilePictureSettingsCard.tsx
@@ -2,7 +2,7 @@ import NextImage from 'next/image';
 import React, {useCallback, useState} from 'react';
 import {useDropzone} from 'react-dropzone';
 import {cn} from 'utils';
-import {Button, Spinner} from 'ui';
+import {Button, CardBase, Spinner} from 'ui';
 import {InputWrapper} from '../../components/Input/Input/InputWrapper';
 import {useUser} from '@clerk/nextjs';
 import {toast} from 'react-hot-toast';
@@ -44,55 +44,57 @@ export function ProfilePictureSettingsCard() {
   }, [user]);
 
   return (
-    <InputWrapper>
-      <div
-        className={cn(
-          'flex flex-col relative justify-center items-center border-2 border-dashed border-gray-300 rounded-3xl bg-gray-100 mb-4 text-gray-400 aspect-video overflow-hidden',
-          'focus:border-gray-500 focus:ring-gray-500',
-          {'bg-gray-200': isDragActive}
-        )}
-      >
-        <div {...getRootProps()} className={cn('h-full w-full')}>
-          <input
-            multiple={false}
-            {...getInputProps()}
-            data-testid="file-input"
-          />
+    <CardBase>
+      <InputWrapper label="Profile picture">
+        <div
+          className={cn(
+            'flex flex-col relative justify-center items-center border-2 border-dashed border-gray-300 rounded-3xl bg-gray-100 text-gray-400 aspect-video overflow-hidden',
+            'focus:border-gray-500 focus:ring-gray-500',
+            {'bg-gray-200': isDragActive}
+          )}
+        >
+          <div {...getRootProps()} className={cn('h-full w-full')}>
+            <input
+              multiple={false}
+              {...getInputProps()}
+              data-testid="file-input"
+            />
 
-          <div className="h-full flex flex-col justify-center items-center gap-y-2">
-            <Button variant="neutral" size="sm">
-              Select file
-            </Button>
-            <p className="text-center">or just drop image here...</p>
-          </div>
-        </div>
-        {isLoading && (
-          <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-gray-100">
-            <Spinner />
-          </div>
-        )}
-        {!isLoading && hasImage && user?.profileImageUrl && (
-          <>
-            <div className="flex items-center justify-center absolute w-full h-full bg-gray-100">
-              <NextImage
-                data-testid="file-input-image-preview"
-                alt=""
-                src={user?.profileImageUrl}
-                fill
-                style={{objectFit: 'contain'}}
-              />
+            <div className="h-full flex flex-col justify-center items-center gap-y-2">
+              <Button variant="neutral" size="sm">
+                Select file
+              </Button>
+              <p className="text-center">or just drop image here...</p>
             </div>
-            <Button
-              variant="neutral"
-              size="sm"
-              className="absolute bottom-5 right-5 z-50"
-              onClick={handleRemoveImage}
-            >
-              Remove
-            </Button>
-          </>
-        )}
-      </div>
-    </InputWrapper>
+          </div>
+          {isLoading && (
+            <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-gray-100">
+              <Spinner />
+            </div>
+          )}
+          {!isLoading && hasImage && user?.profileImageUrl && (
+            <>
+              <div className="flex items-center justify-center absolute w-full h-full bg-gray-100">
+                <NextImage
+                  data-testid="file-input-image-preview"
+                  alt=""
+                  src={user?.profileImageUrl}
+                  fill
+                  style={{objectFit: 'contain'}}
+                />
+              </div>
+              <Button
+                variant="neutral"
+                size="sm"
+                className="absolute bottom-5 right-5 z-50"
+                onClick={handleRemoveImage}
+              >
+                Remove
+              </Button>
+            </>
+          )}
+        </div>
+      </InputWrapper>
+    </CardBase>
   );
 }
