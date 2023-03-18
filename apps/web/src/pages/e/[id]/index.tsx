37b62c1b0db8d9b252a7462f0c791bb158d31db9
Ilya Daraseliya <idarase@gmail.com>
2023-03-18 07:45:19 +0100

    ssr event

--- apps/web/src/pages/e/[id]/index.tsx
+++ apps/web/src/pages/e/[id]/index.tsx
@@ -1,11 +1,15 @@
 import {GetServerSidePropsContext, InferGetServerSidePropsType} from 'next';
 import Head from 'next/head';
-import {useMemo} from 'react';
+import {createProxySSGHelpers} from '@trpc/react-query/ssg';
 import {AdminSection} from '../../../features/AdminSection/AdminSection';
 import {EventView} from '../../../features/EventView/EventView';
 import {Container, LoadingBlock, Spinner} from 'ui';
 import {trpc} from 'trpc/src/trpc';
 import {useEventId, useHandleEmailCallbackParam, useMe} from 'hooks';
+import {appRouter} from 'trpc/src/routers/_app';
+import {createContext} from 'trpc';
+import SuperJSON from 'superjson';
+import {ONE_WEEK_IN_SECONDS} from '../../../constants';
 
 export default function EventPage() {
   useHandleEmailCallbackParam();
@@ -55,4 +59,28 @@ export default function EventPage() {
       )}
     </>
   );
+}
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{id: string}>
+) {
+  const ssg = createProxySSGHelpers({
+    router: appRouter,
+    ctx: await createContext(),
+    transformer: SuperJSON,
+  });
+  const id = context.params?.id as string;
+  await ssg.event.getByShortId.prefetch({id});
+
+  context.res.setHeader(
+    'Cache-Control',
+    `s-maxage=10, stale-while-revalidate=${ONE_WEEK_IN_SECONDS}`
+  );
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
 }
