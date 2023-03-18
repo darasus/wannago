8f4285a5faed2f447837bc6fd616f3bf789214a6
Ilya Daraseliya <idarase@gmail.com>
2023-03-18 22:07:18 +0100

    settings page instead of me

---
+++ apps/web/src/pages/settings.tsx
@@ -0,0 +1,29 @@
+import {useUser} from '@clerk/nextjs';
+import Head from 'next/head';
+import {Container, PageHeader, Text} from 'ui';
+import {EmailSettingsCard} from '../features/UserSettings/EmailSettingsCard';
+import {NameSettingsCard} from '../features/UserSettings/NameSettingsCard';
+import {ProfilePictureSettingsCard} from '../features/UserSettings/ProfilePictureSettingsCard';
+import {withProtected} from '../utils/withAuthProtect';
+
+function ProfilePage() {
+  const {user} = useUser();
+
+  return (
+    <>
+      <Head>
+        <title>{`${user?.fullName} | WannaGo`}</title>
+      </Head>
+      <Container maxSize="sm">
+        <div className="flex flex-col gap-y-4">
+          <PageHeader title="Settings" />
+          <NameSettingsCard />
+          <ProfilePictureSettingsCard />
+          <EmailSettingsCard />
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default withProtected(ProfilePage);
