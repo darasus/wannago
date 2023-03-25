16edd20c5a71cd33f3325690b3937ce3e46f2e44
Ilya Daraseliya <idarase@gmail.com>
2023-03-25 21:32:41 +0100

    reduce transaction sampling

--- apps/web/sentry.edge.config.js
+++ apps/web/sentry.edge.config.js
@@ -8,7 +8,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 init({
   dsn: SENTRY_DSN,
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 0.1,
   enabled: process.env.VERCEL_ENV === 'production',
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
