817322277b7a30de6bf2a890013b4a326feb9141
Ilya Daraseliya <idarase@gmail.com>
2023-03-28 08:51:03 +0200

    add team tests

---
+++ apps/web-e2e/cypress/integration/team.cy.js
@@ -0,0 +1,31 @@
+describe('Team', () => {
+  it('can create team', () => {
+    cy.login();
+    cy.visit('/settings');
+    cy.get('[data-testid="grouped-toggle-team"]').click();
+
+    cy.document().then(doc => {
+      if (doc.querySelector('[data-testid="team-settings-remove-button"]')) {
+        cy.get('[data-testid="team-settings-remove-button"]').click();
+        cy.get('[data-testid="confirm-dialog-confirm-button"]').click();
+        cy.get('[data-testid="toast-success"]');
+        cy.wait(3000);
+        cy.get('[data-testid="team-settings-form-input-name"]').should(
+          'be.empty'
+        );
+      }
+    });
+
+    cy.get('[data-testid="team-settings-form-input-name"]').type('Test team');
+    cy.get('[data-testid="team-settings-form-input-email"]').type(
+      'email@email.com'
+    );
+    cy.get('[data-testid="file-input"]').selectFile(
+      'cypress/support/logo.png',
+      {force: true}
+    );
+    cy.wait(1000);
+    cy.get('[data-testid="file-input-image-preview"]');
+    cy.get('[data-testid="team-settings-form-input-submit-button"]').click();
+  });
+});
