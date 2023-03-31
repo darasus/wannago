70c63e786bf3a364ab198234dbae0f5f585de0e6
Ilya Daraseliya <idarase@gmail.com>
2023-03-31 07:19:40 +0200

    integrate sentry replays

--- apps/web/sentry.client.config.js
+++ apps/web/sentry.client.config.js
@@ -1,8 +1,4 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import {init} from '@sentry/nextjs';
+import {init, Replay} from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
@@ -10,7 +6,7 @@ init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 0.1,
   enabled: process.env.VERCEL_ENV === 'production',
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  replaysSessionSampleRate: 1.0,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [new Replay()],
 });
