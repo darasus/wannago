cd99861a255d28d85fb43b094726a758a4e315d6
Ilya Daraseliya <idarase@gmail.com>
2023-04-02 16:22:47 +0200

    update user settings

--- apps/web/src/features/UserSettings/UserSettings.tsx
+++ apps/web/src/features/UserSettings/UserSettings.tsx
@@ -1,6 +1,8 @@
 import {useMyUserQuery, useUpdateUserMutation} from 'hooks';
+import Link from 'next/link';
 import {FormProvider, useForm} from 'react-hook-form';
-import {Button, CardBase} from 'ui';
+import {trpc} from 'trpc/src/trpc';
+import {Button, CardBase, Text} from 'ui';
 import {FileInput} from '../../components/Input/FileInput/FileInput';
 import {Input} from '../../components/Input/Input/Input';
 
@@ -12,6 +14,8 @@ interface UserForm {
 }
 
 export function UserSettings() {
+  const subscription = trpc.subscription.createProSubscriptionLink.useQuery();
+  const mySubscription = trpc.subscription.getMySubscription.useQuery();
   const user = useMyUserQuery();
   const updateUser = useUpdateUserMutation();
   const form = useForm<UserForm>({
@@ -36,66 +40,83 @@ export function UserSettings() {
   });
 
   return (
-    <CardBase>
-      <form onSubmit={onSubmit}>
-        <div className="flex flex-col gap-y-4">
-          <div>
-            <Input
-              type="text"
-              label="First name"
-              {...form.register('firstName', {
-                required: {
-                  value: true,
-                  message: 'First name is required',
-                },
-              })}
-              data-testid="first-name-input"
-            />
+    <div className="flex flex-col gap-4">
+      <CardBase>
+        <form onSubmit={onSubmit}>
+          <div className="flex flex-col gap-y-4">
+            <div>
+              <Input
+                type="text"
+                label="First name"
+                {...form.register('firstName', {
+                  required: {
+                    value: true,
+                    message: 'First name is required',
+                  },
+                })}
+                data-testid="first-name-input"
+              />
+            </div>
+            <div>
+              <Input
+                type="text"
+                label="Last name"
+                {...form.register('lastName', {
+                  required: {
+                    value: true,
+                    message: 'Last name is required',
+                  },
+                })}
+                data-testid="last-name-input"
+              />
+            </div>
+            <div>
+              <Input
+                type="email"
+                label="Email"
+                {...form.register('email', {
+                  required: {
+                    value: true,
+                    message: 'Email is required',
+                  },
+                })}
+                data-testid="email-input"
+                disabled
+              />
+            </div>
+            <div>
+              <FormProvider {...form}>
+                <FileInput {...form.register('profileImageSrc')} />
+              </FormProvider>
+            </div>
+            <div>
+              <Button
+                type="submit"
+                disabled={form.formState.isSubmitting}
+                data-testid="user-settings-submit-button"
+                isLoading={form.formState.isSubmitting}
+              >
+                Save
+              </Button>
+            </div>
           </div>
+        </form>
+      </CardBase>
+      <CardBase>
+        <Button as="a" href={subscription.data?.url || ''}>
+          Become pro
+        </Button>
+      </CardBase>
+      {mySubscription.data && (
+        <CardBase>
           <div>
-            <Input
-              type="text"
-              label="Last name"
-              {...form.register('lastName', {
-                required: {
-                  value: true,
-                  message: 'Last name is required',
-                },
-              })}
-              data-testid="last-name-input"
-            />
-          </div>
-          <div>
-            <Input
-              type="email"
-              label="Email"
-              {...form.register('email', {
-                required: {
-                  value: true,
-                  message: 'Email is required',
-                },
-              })}
-              data-testid="email-input"
-              disabled
-            />
-          </div>
-          <div>
-            <FormProvider {...form}>
-              <FileInput {...form.register('profileImageSrc')} />
-            </FormProvider>
-          </div>
-          <div>
-            <Button
-              type="submit"
-              disabled={form.formState.isSubmitting}
-              data-testid="user-settings-submit-button"
-              isLoading={form.formState.isSubmitting}
-            >
-              Save
+            <Text>{mySubscription.data.subscription?.type}</Text>
+            <Button as="a" href={mySubscription.data.customerPortalUrl}>
+              Update billing
             </Button>
           </div>
-        </div>
-      </form>
-    </CardBase>
+        </CardBase>
+      )}
+    </div>
   );
 }
