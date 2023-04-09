8cdefbc56e2eae5c7ed20f7377154c8c10588cc2
Ilya Daraseliya <idarase@gmail.com>
2023-04-09 07:34:10 +0200

    move login/register into features

---
+++ apps/web/src/features/Login/Login.tsx
@@ -0,0 +1,239 @@
+import {useSignIn} from '@clerk/nextjs';
+import {ClerkAPIError} from '@clerk/types';
+import {useMyUserQuery} from 'hooks';
+import {useRouter} from 'next/router';
+import {useCallback, useEffect, useState} from 'react';
+import {FormProvider, useForm, useFormContext} from 'react-hook-form';
+import {CardBase, LoadingBlock, Button, Text} from 'ui';
+import {cn, sleep} from 'utils';
+import {Input} from '../../components/Input/Input/Input';
+import {titleFont} from '../../fonts';
+
+interface TEmailForm {
+  email: string;
+}
+
+interface TCodeForm {
+  code: string;
+}
+
+export interface APIResponseError {
+  errors: ClerkAPIError[];
+}
+
+interface Props {
+  onDone?: () => void;
+}
+
+export function Login({onDone}: Props) {
+  const me = useMyUserQuery();
+  const {setSession, isLoaded} = useSignIn();
+  const router = useRouter();
+  const [step, setStep] = useState<'email' | 'code'>('email');
+  const emailForm = useForm<TEmailForm>();
+  const codeForm = useForm<TCodeForm>({
+    mode: 'onSubmit',
+  });
+
+  const ready = useCallback(async (): Promise<any> => {
+    const {data} = await me.refetch();
+
+    if (!data?.id) {
+      await sleep(1000);
+      return ready();
+    }
+  }, [me]);
+
+  const handleOnDone = useCallback(
+    async (createdSessionId: string) => {
+      await setSession?.(createdSessionId);
+      await ready();
+
+      onDone?.();
+    },
+    [ready, setSession, onDone]
+  );
+
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <Text className={cn('text-4xl', titleFont.className)}>Login</Text>
+      <CardBase className="w-full">
+        {isLoaded ? (
+          <>
+            {step === 'email' && (
+              <FormProvider {...emailForm}>
+                <EmailForm goToNextStep={() => setStep('code')} />
+              </FormProvider>
+            )}
+            {step === 'code' && (
+              <FormProvider {...codeForm}>
+                <CodeForm
+                  onDone={handleOnDone}
+                  email={emailForm.watch('email')}
+                />
+              </FormProvider>
+            )}
+          </>
+        ) : (
+          <LoadingBlock />
+        )}
+      </CardBase>
+      <Text className="text-center">- OR -</Text>
+      <Button as="a" href="/register" variant="neutral">
+        Create account
+      </Button>
+    </div>
+  );
+}
+
+interface EmailFormProps {
+  goToNextStep: () => void;
+}
+
+function EmailForm({goToNextStep}: EmailFormProps) {
+  const form = useFormContext<TEmailForm>();
+  const {signIn} = useSignIn();
+
+  const submit = form.handleSubmit(async data => {
+    try {
+      await signIn?.create({
+        identifier: data.email,
+        strategy: 'email_code',
+      });
+      goToNextStep();
+    } catch (error: any) {
+      const e = error as APIResponseError;
+
+      form.setError('email', {
+        type: 'manual',
+        message: parseError(e),
+      });
+    }
+  });
+
+  useEffect(() => {
+    form.setFocus('email');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <form onSubmit={submit}>
+      <div className="flex flex-col gap-4">
+        <Input
+          type="email"
+          label="Email"
+          {...form.register('email', {
+            required: {
+              value: true,
+              message: 'Email is required',
+            },
+          })}
+          error={form.formState.errors.email}
+          autoComplete="email"
+          data-testid="login-email-input"
+        />
+        <Button
+          type="submit"
+          isLoading={form.formState.isSubmitting}
+          data-testid="login-email-form-submit"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+interface CodeFormProps {
+  email: string;
+  onDone: (createdSessionId: string) => Promise<void>;
+}
+
+function CodeForm({onDone, email}: CodeFormProps) {
+  const form = useFormContext<TCodeForm>();
+  const {signIn} = useSignIn();
+
+  const submit = form.handleSubmit(async data => {
+    try {
+      const signInAttempt = await signIn?.attemptFirstFactor({
+        strategy: 'email_code',
+        code: data.code,
+      });
+
+      if (
+        signInAttempt?.status === 'complete' &&
+        signInAttempt?.createdSessionId
+      ) {
+        await onDone(signInAttempt?.createdSessionId);
+      }
+    } catch (error: any) {
+      form.setError('code', {
+        type: 'manual',
+        message: parseError(error as APIResponseError),
+      });
+    }
+  });
+
+  useEffect(() => {
+    form.setFocus('code');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <form onSubmit={submit}>
+      <div className="flex flex-col gap-4">
+        <Input
+          type="number"
+          description={`Enter the code sent to ${email}`}
+          label="Code"
+          {...form.register('code', {
+            validate: value => {
+              try {
+                if (!value) return 'Code is required';
+
+                if (typeof value === 'string') {
+                  const length = value.length;
+                  if (length > 6 || length < 6) {
+                    return 'Code must be 6 characters long';
+                  }
+                }
+                const n = Number(value);
+
+                if (typeof n === 'number' && !isNaN(n)) {
+                  return true;
+                } else {
+                  return 'Code must be a number';
+                }
+              } catch (error) {
+                return 'Code must be a number';
+              }
+            },
+          })}
+          error={form.formState.errors.code}
+          autoComplete="off"
+          data-testid="login-code-input"
+        />
+
+        <Button
+          type="submit"
+          isLoading={form.formState.isSubmitting}
+          data-testid="login-code-form-submit"
+        >
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+}
+
+export function parseError(err: APIResponseError): string {
+  if (!err) {
+    return '';
+  }
+
+  if (err.errors) {
+    return err.errors[0].longMessage || '';
+  }
+
+  throw err;
+}
--- apps/web/src/pages/login.page.tsx
+++ apps/web/src/pages/login.page.tsx
@@ -1,55 +1,15 @@
-import {useSignIn} from '@clerk/nextjs';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
-import {useCallback, useEffect, useState} from 'react';
-import {FormProvider, useForm, useFormContext} from 'react-hook-form';
-import {Button, CardBase, Container, LoadingBlock, Text} from 'ui';
-import {cn, sleep} from 'utils';
-import {useMyUserQuery} from 'hooks';
-import {Input} from '../components/Input/Input/Input';
-import {titleFont} from '../fonts';
-import type {ClerkAPIError} from '@clerk/types';
-
-interface TEmailForm {
-  email: string;
-}
-
-interface TCodeForm {
-  code: string;
-}
-
-export interface APIResponseError {
-  errors: ClerkAPIError[];
-}
+import {Container} from 'ui';
+import {Login} from '../features/Login/Login';
+import {useCallback} from 'react';
 
 function LoginPage() {
-  const me = useMyUserQuery();
-  const {setSession, isLoaded} = useSignIn();
   const router = useRouter();
-  const [step, setStep] = useState<'email' | 'code'>('email');
-  const emailForm = useForm<TEmailForm>();
-  const codeForm = useForm<TCodeForm>({
-    mode: 'onSubmit',
-  });
 
-  const ready = useCallback(async (): Promise<any> => {
-    const {data} = await me.refetch();
-
-    if (!data?.id) {
-      await sleep(1000);
-      return ready();
-    }
-  }, [me]);
-
-  const handleOnDone = useCallback(
-    async (createdSessionId: string) => {
-      await setSession?.(createdSessionId);
-      await ready();
-
-      router.push('/dashboard');
-    },
-    [ready, router, setSession]
-  );
+  const handleOnDone = useCallback(() => {
+    router.push('/dashboard');
+  }, [router]);
 
   return (
     <>
@@ -57,189 +17,10 @@ function LoginPage() {
         <title>Login | WannaGo</title>
       </Head>
       <Container maxSize="xs" className="py-4">
-        <div className="flex flex-col items-center gap-4">
-          <Text className={cn('text-4xl', titleFont.className)}>Login</Text>
-          <CardBase className="w-full">
-            {isLoaded ? (
-              <>
-                {step === 'email' && (
-                  <FormProvider {...emailForm}>
-                    <EmailForm goToNextStep={() => setStep('code')} />
-                  </FormProvider>
-                )}
-                {step === 'code' && (
-                  <FormProvider {...codeForm}>
-                    <CodeForm
-                      onDone={handleOnDone}
-                      email={emailForm.watch('email')}
-                    />
-                  </FormProvider>
-                )}
-              </>
-            ) : (
-              <LoadingBlock />
-            )}
-          </CardBase>
-          <Text className="text-center">- OR -</Text>
-          <Button as="a" href="/register">
-            Create account
-          </Button>
-        </div>
+        <Login onDone={handleOnDone} />
       </Container>
     </>
   );
 }
 
 export default LoginPage;
-
-interface EmailFormProps {
-  goToNextStep: () => void;
-}
-
-function EmailForm({goToNextStep}: EmailFormProps) {
-  const form = useFormContext<TEmailForm>();
-  const {signIn} = useSignIn();
-
-  const submit = form.handleSubmit(async data => {
-    try {
-      await signIn?.create({
-        identifier: data.email,
-        strategy: 'email_code',
-      });
-      goToNextStep();
-    } catch (error: any) {
-      const e = error as APIResponseError;
-
-      form.setError('email', {
-        type: 'manual',
-        message: parseError(e),
-      });
-    }
-  });
-
-  useEffect(() => {
-    form.setFocus('email');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <form onSubmit={submit}>
-      <div className="flex flex-col gap-4">
-        <Input
-          type="email"
-          label="Email"
-          {...form.register('email', {
-            required: {
-              value: true,
-              message: 'Email is required',
-            },
-          })}
-          error={form.formState.errors.email}
-          autoComplete="email"
-          data-testid="login-email-input"
-        />
-        <Button
-          type="submit"
-          isLoading={form.formState.isSubmitting}
-          data-testid="login-email-form-submit"
-        >
-          Submit
-        </Button>
-      </div>
-    </form>
-  );
-}
-
-interface CodeFormProps {
-  email: string;
-  onDone: (createdSessionId: string) => Promise<void>;
-}
-
-function CodeForm({onDone, email}: CodeFormProps) {
-  const form = useFormContext<TCodeForm>();
-  const {signIn} = useSignIn();
-
-  const submit = form.handleSubmit(async data => {
-    try {
-      const signInAttempt = await signIn?.attemptFirstFactor({
-        strategy: 'email_code',
-        code: data.code,
-      });
-
-      if (
-        signInAttempt?.status === 'complete' &&
-        signInAttempt?.createdSessionId
-      ) {
-        onDone(signInAttempt?.createdSessionId);
-      }
-    } catch (error: any) {
-      form.setError('code', {
-        type: 'manual',
-        message: parseError(error as APIResponseError),
-      });
-    }
-  });
-
-  useEffect(() => {
-    form.setFocus('code');
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <form onSubmit={submit}>
-      <div className="flex flex-col gap-4">
-        <Input
-          type="number"
-          description={`Enter the code sent to ${email}`}
-          label="Code"
-          {...form.register('code', {
-            validate: value => {
-              try {
-                if (!value) return 'Code is required';
-
-                if (typeof value === 'string') {
-                  const length = value.length;
-                  if (length > 6 || length < 6) {
-                    return 'Code must be 6 characters long';
-                  }
-                }
-                const n = Number(value);
-
-                if (typeof n === 'number' && !isNaN(n)) {
-                  return true;
-                } else {
-                  return 'Code must be a number';
-                }
-              } catch (error) {
-                return 'Code must be a number';
-              }
-            },
-          })}
-          error={form.formState.errors.code}
-          autoComplete="off"
-          data-testid="login-code-input"
-        />
-
-        <Button
-          type="submit"
-          isLoading={form.formState.isSubmitting}
-          data-testid="login-code-form-submit"
-        >
-          Submit
-        </Button>
-      </div>
-    </form>
-  );
-}
-
-export function parseError(err: APIResponseError): string {
-  if (!err) {
-    return '';
-  }
-
-  if (err.errors) {
-    return err.errors[0].longMessage || '';
-  }
-
-  throw err;
-}
