410e35e7cc20415e2b178bda506b89b5cc644ed9
Ilya Daraseliya <idarase@gmail.com>
2023-04-13 08:11:03 +0200

    fix team test

--- apps/web-e2e/cypress/integration/team-settings.cy.js
+++ apps/web-e2e/cypress/integration/team-settings.cy.js
@@ -3,23 +3,9 @@ describe('Team', () => {
     cy.login();
     cy.visit('/settings/personal');
     cy.get('[data-testid="team-settings-button"]').click();
-
-    cy.document().then(doc => {
-      if (doc.querySelector('[data-testid="team-settings-remove-button"]')) {
-        cy.get('[data-testid="team-settings-remove-button"]').click();
-        cy.get('[data-testid="confirm-dialog-confirm-button"]').click();
-        cy.get('[data-testid="toast-success"]').should('be.visible');
-        cy.get('[data-testid="team-settings-form-input-name"]').should(
-          'be.empty'
-        );
-      }
-    });
-
-    cy.wait(1000);
-
-    cy.get('[data-testid="team-settings-form-input-name"]').type(
-      'Organization 1'
-    );
+    cy.get('[data-testid="team-settings-form-input-name"]')
+      .clear()
+      .type('Organization 1');
     cy.get('[data-testid="team-settings-form-input-email"]')
       .clear()
       .type(Cypress.env('organization_1_email'));
