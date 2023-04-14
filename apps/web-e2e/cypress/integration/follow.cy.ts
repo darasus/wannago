acc9a12db05e5c838f04f76d9020cbf9b6e5d567
Ilya Daraseliya <idarase@gmail.com>
2023-04-14 08:28:49 +0200

    fix test

--- apps/web-e2e/cypress/integration/follow.cy.ts
+++ apps/web-e2e/cypress/integration/follow.cy.ts
@@ -8,11 +8,13 @@ describe('Follow', () => {
     cy.visit('/dashboard');
     cy.toggleSession();
     cy.createOfflineEvent();
+    cy.publishCurrentEvent();
     cy.logout();
     cy.login('user_1_email');
     cy.visit(`/o/${Cypress.env('organization_2_id')}`);
     cy.get('[data-testid="follow-button"]').click();
     cy.get('[data-testid="follower-count"]').should('have.text', '1');
+    cy.wait(2000);
     cy.visit('/dashboard');
     cy.get('[data-testid="select-button"]').click();
     cy.get('[data-testid="select-option-button"]')
@@ -28,11 +30,13 @@ describe('Follow', () => {
     cy.login('user_2_email');
     cy.visit('/dashboard');
     cy.createOfflineEvent();
+    cy.publishCurrentEvent();
     cy.logout();
     cy.login('user_1_email');
     cy.visit(`/u/${Cypress.env('user_2_id')}`);
     cy.get('[data-testid="follow-button"]').click();
     cy.get('[data-testid="follower-count"]').should('have.text', '1');
+    cy.wait(2000);
     cy.visit('/dashboard');
     cy.get('[data-testid="select-button"]').click();
     cy.get('[data-testid="select-option-button"]')
