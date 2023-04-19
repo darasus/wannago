8a2d66631b6362f8fdceeaac4e71db35779f2138
Ilya Daraseliya <idarase@gmail.com>
2023-04-19 07:39:06 +0200

    saving progress

--- apps/web/src/pages/api/ai.api.ts
+++ apps/web/src/pages/api/ai.api.ts
@@ -10,14 +10,13 @@ import {StructuredOutputParser} from 'langchain/output_parsers';
 const generateEventData = async (userInstruction: string) => {
   const parser = StructuredOutputParser.fromNamesAndDescriptions({
     title:
-      "Based on the prompt generate catchy title for the user's event. Be very creative, use more words than necessary.",
+      "Based on the user prompt generate catchy title for the user's event. Be very creative, use more words than necessary.",
     description:
-      "Generate catchy description for the user's event. Be very creative, use more words than necessary. If you use names then prefer placeholders like {name} instead of actual names.",
-    startDate: `Current day is ${new Date().toISOString()}. Start date that was indicated in the user's prompt, needs to be ISO 8601 format. If year or month not mentioned then use current year and month.`,
-    endDate: `Current day is ${new Date().toISOString()}. End date that was indicated in the user's prompt, needs to be ISO 8601 format. If year or month not mentioned then use current year and month.`,
-    address: "address that was indicated in the user's prompt",
-    maxNumberOfAttendees:
-      "Maximum number of attendees that was indicated in the user's prompt. If not mentioned then set to '0'.",
+      "Generate catchy description for the user's event. Be very creative, use more words than necessary. If you use names then prefer placeholders like {name} instead of actual names. {name} is always lowercased.",
+    startDate: `Current day is ${new Date().toISOString()}. Parse the date in user prompt considering user is in Amsterdam timezone. Start date that was indicated in the user's prompt, needs to be ISO 8601 format. If not mentioned then set to "unknown".`,
+    endDate: `Current day is ${new Date().toISOString()}. Parse the date in user prompt considering user is in Amsterdam timezone. If end date was not indicated in the user's prompt derive approximate end date from start date, needs to be ISO 8601 format.`,
+    address: `Address that was indicated in the user's prompt. If location is not provided then use "unknown".`,
+    maxNumberOfAttendees: `Maximum number of attendees that was indicated in the user's prompt. If not mentioned then set to "0".`,
   });
   const formatInstructions = parser.getFormatInstructions();
   const prompt = new PromptTemplate({
@@ -28,9 +27,9 @@ const generateEventData = async (userInstruction: string) => {
     inputVariables: ['instruction'],
     partialVariables: {format_instructions: formatInstructions},
   });
-  const model = new OpenAI({temperature: 0});
+  const model = new OpenAI({temperature: 0, modelName: 'gpt-3.5-turbo'});
   const input = await prompt.format({
-    instruction: userInstruction,
+    instruction: 'User prompt: ' + userInstruction,
   });
   const response = await model.call(input);
   const data = await parser.parse(response);
@@ -43,6 +42,8 @@ export default async function handler(
 ) {
   const userInstruction = req.query.prompt as string;
   const data = await generateEventData(userInstruction);
+
+  console.log(JSON.stringify(data, null, 2));
 
   res.status(200).json({userInstruction, output: data});
 }
