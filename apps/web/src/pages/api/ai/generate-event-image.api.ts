e6f1b02b4566919de72205286ba775e02e8a68e7
Ilya Daraseliya <idarase@gmail.com>
2023-04-23 07:37:24 +0200

    add console.log

--- apps/web/src/pages/api/ai/generate-event-image.api.ts
+++ apps/web/src/pages/api/ai/generate-event-image.api.ts
@@ -90,6 +90,7 @@ export default async function handler(
 
     res.status(200).json({url, imageSrcBase64, height, width});
   } catch (error) {
+    console.log(error);
     captureException(error);
     res.status(400).json(error);
   }
