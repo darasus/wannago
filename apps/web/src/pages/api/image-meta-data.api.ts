ac6a9b6b21eeb060f6d57842a183664e55d4906c
Ilya Daraseliya <idarase@gmail.com>
2023-04-23 07:40:46 +0200

    remove method check

--- apps/web/src/pages/api/image-meta-data.api.ts
+++ apps/web/src/pages/api/image-meta-data.api.ts
@@ -13,10 +13,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== 'POST') {
-    res.status(405).json({error: 'Method Not Allowed'});
-  }
-
   captureMessage('Generating image metadata', {
     extra: {
       body: JSON.stringify(req.body),
