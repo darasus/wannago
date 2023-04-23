e8cc6269f2908b6bd7c0b30f2954f783223bc8f1
Ilya Daraseliya <idarase@gmail.com>
2023-04-23 08:13:04 +0200

    fix image upload

--- apps/web/src/pages/api/upload-image.api.ts
+++ apps/web/src/pages/api/upload-image.api.ts
@@ -2,7 +2,7 @@ import {NextApiRequest, NextApiResponse} from 'next';
 import fs from 'fs';
 import FormData from 'form-data';
 import formidable from 'formidable';
-import got from 'got';
+import axios from 'axios';
 import {env} from 'server-env';
 import {getImageMetaData} from 'utils';
 import sharp from 'sharp';
@@ -35,19 +35,17 @@ export default async function handler(
     payload.append('file', newBuffer, fileName);
 
     try {
-      const response = (await got
-        .post(
-          'https://api.cloudflare.com/client/v4/accounts/520ed574991657981b4927dda46f2477/images/v1',
-          {
-            body: payload,
-            headers: {
-              Authorization: `Bearer ${env.CLOUDFLARE_API_KEY}`,
-            },
-          }
-        )
-        .json()) as any;
+      const response = await axios.post(
+        'https://api.cloudflare.com/client/v4/accounts/520ed574991657981b4927dda46f2477/images/v1',
+        payload,
+        {
+          headers: {
+            Authorization: `Bearer ${env.CLOUDFLARE_API_KEY}`,
+          },
+        }
+      );
 
-      const url = response?.result?.variants[0] as string;
+      const url = response?.data?.result?.variants[0] as string;
       const {height, width, imageSrcBase64} = await getImageMetaData(url);
 
       res.status(200).json({url, imageSrcBase64, height, width});
