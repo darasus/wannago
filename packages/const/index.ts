d257aa0c44d5e1700a445b4a091b976db5217d0e
Ilya Daraseliya <idarase@gmail.com>
2023-04-25 07:23:07 +0200

    update examples

--- packages/const/index.ts
+++ packages/const/index.ts
@@ -1,11 +1,9 @@
 export const REMINDER_PERIOD_IN_SECONDS = 60 * 60 * 3; // 3 hours
 export const ONE_WEEK_IN_SECONDS = 60 * 60 * 24 * 7;
 export const exampleEventIds = [
-  'FyThYG',
-  'WC1Fo6',
-  '59AlAn',
-  '0q5ipF',
-  'AfnfDh',
-  '3ggj1Y',
+  // prod
+  '7WPjci',
+  'rv3f92',
+  // dev
   'pg15re',
 ];
