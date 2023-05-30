8a114614c63f0b7dd45ea22f904b8e70f64e55b2
Ilya Daraseliya <idarase@gmail.com>
2023-05-30 20:24:51 +0200

    upgrade sentry and enable profiling

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -1,9 +1,12 @@
 import * as Sentry from '@sentry/nextjs';
+import {ProfilingIntegration} from '@sentry/profiling-node';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
   tracesSampleRate: 0.1,
+  profilesSampleRate: 0.1,
   enabled: process.env.VERCEL_ENV === 'production',
+  integrations: [new ProfilingIntegration()],
 });
