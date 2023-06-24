4d8140376d8c97d24b46998a2ec55e885de40cae
Ilya Daraseliya <idarase@gmail.com>
2023-06-24 07:42:45 +0200

    fix build

--- packages/trpc/src/routers/event/handlers/getPublicEvents.ts
+++ packages/trpc/src/routers/event/handlers/getPublicEvents.ts
@@ -5,7 +5,7 @@ export const getPublicEvents = publicProcedure
   .input(z.object({id: z.string().uuid()}))
   .query(async ({ctx, input}) => {
     return ctx.actions.getEvents({
-      id: input.id,
+      authorIds: [input.id],
       isPublished: true,
       eventType: 'organizing',
     });
