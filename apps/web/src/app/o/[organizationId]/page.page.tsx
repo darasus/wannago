f7efa9727bc7cfa428017e627084335a51951dd9
Ilya Daraseliya <idarase@gmail.com>
2023-06-25 14:00:18 +0200

    migrate organization page

---
+++ apps/web/src/app/o/[organizationId]/page.page.tsx
@@ -0,0 +1,63 @@
+import {PublicProfile} from '../../(features)/PublicProfile/PublicProfile';
+import {api} from '../../../trpc/server';
+
+export const generateMetadata = async ({
+  params: {organizationId},
+}: {
+  params: {organizationId: string};
+}) => {
+  const organization = await api.organization.getOrganizationById.query({
+    organizationId,
+  });
+
+  if (!organization) {
+    return null;
+  }
+
+  return {
+    title: `${organization.name} | WannaGo`,
+  };
+};
+
+export default async function ProfilePage({
+  params: {organizationId},
+}: {
+  params: {organizationId: string};
+}) {
+  const [
+    organizationPromise,
+    eventsPromise,
+    followCountsPromise,
+    amFollowingPromise,
+  ] = [
+    api.organization.getOrganizationById.query({
+      organizationId,
+    }),
+    api.event.getPublicEvents.query({
+      id: organizationId,
+    }),
+    api.follow.getFollowCounts.query({
+      organizationId,
+    }),
+    api.follow.amFollowing.query({
+      organizationId,
+    }),
+  ];
+
+  const organization = await organizationPromise;
+  const events = await eventsPromise;
+
+  if (!organization) {
+    return null;
+  }
+
+  return (
+    <PublicProfile
+      events={events || []}
+      name={`${organization.name}`}
+      profileImageSrc={organization.logoSrc}
+      followCountsPromise={followCountsPromise}
+      amFollowingPromise={amFollowingPromise}
+    />
+  );
+}
