d7e3df2fd228b0b382ed69f19e05e204c33cedf9
Ilya Daraseliya <idarase@gmail.com>
2023-06-27 11:48:01 +0200

    fix router

--- packages/api/src/edge.ts
+++ packages/api/src/edge.ts
@@ -1,27 +1,18 @@
 import {createTRPCRouter} from './trpc';
 import {eventRouter} from './routers/event';
-import {mapsRouter} from './routers/maps';
 import {mailRouter} from './routers/mail';
 import {adminRouter} from './routers/admin';
 import {userRouter} from './routers/user';
 import {organizationRouter} from './routers/organization';
-import {subscriptionRouter} from './routers/subscription';
 import {conversationRouter} from './routers/conversation';
 import {followRouter} from './routers/follow';
-import {paymentsRouter} from './routers/payments';
-import {stripeAccountLinkRouter} from './routers/stripeAccountLink';
 
-// Deployed to /trpc/edge/**
 export const edgeRouter = createTRPCRouter({
   event: eventRouter,
-  maps: mapsRouter,
   mail: mailRouter,
   admin: adminRouter,
   user: userRouter,
   organization: organizationRouter,
-  subscriptionPlan: subscriptionRouter,
   follow: followRouter,
-  payments: paymentsRouter,
-  stripeAccountLink: stripeAccountLinkRouter,
   conversation: conversationRouter,
 });
--- packages/api/src/lambda.ts
+++ packages/api/src/lambda.ts
@@ -1,27 +1,12 @@
 import {createTRPCRouter} from './trpc';
-import {eventRouter} from './routers/event';
 import {mapsRouter} from './routers/maps';
-import {mailRouter} from './routers/mail';
-import {adminRouter} from './routers/admin';
-import {userRouter} from './routers/user';
-import {organizationRouter} from './routers/organization';
 import {subscriptionRouter} from './routers/subscription';
-import {conversationRouter} from './routers/conversation';
-import {followRouter} from './routers/follow';
 import {paymentsRouter} from './routers/payments';
 import {stripeAccountLinkRouter} from './routers/stripeAccountLink';
 
-// Deployed to /trpc/lambda/**
 export const lambdaRouter = createTRPCRouter({
-  event: eventRouter,
   maps: mapsRouter,
-  mail: mailRouter,
-  admin: adminRouter,
-  user: userRouter,
-  organization: organizationRouter,
   subscriptionPlan: subscriptionRouter,
-  follow: followRouter,
   payments: paymentsRouter,
   stripeAccountLink: stripeAccountLinkRouter,
-  conversation: conversationRouter,
 });
