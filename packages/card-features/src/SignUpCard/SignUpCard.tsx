6600826850be291caa0e32f521946d72d38a3424
Ilya Daraseliya <idarase@gmail.com>
2023-07-02 09:40:10 +0200

    fix event fetch

--- packages/card-features/src/SignUpCard/SignUpCard.tsx
+++ packages/card-features/src/SignUpCard/SignUpCard.tsx
@@ -1,13 +1,12 @@
 'use client';
 
 import {Event, Ticket} from '@prisma/client';
-import {SignUpCard as _SignUpCard} from 'cards';
-import {Container} from 'ui';
+import {CardBase, Container, Text} from 'ui';
 import {FreeEventAction} from './features/FreeEventAction/FreeEventAction';
 import {PaidEventAction} from './features/PaidEventAction/PaidEventAction';
 import {api} from '../../../../apps/web/src/trpc/client';
-import {use} from 'react';
 import {RouterOutputs} from 'api';
+import {Suspense, use} from 'react';
 
 interface Props {
   event: Event & {tickets: Ticket[]};
@@ -16,29 +15,44 @@ interface Props {
 }
 
 export function SignUpCard({event, myTicketPromise, mePromise}: Props) {
-  const attendeeCount = use(
-    api.event.getNumberOfAttendees.query({
-      eventId: event.id,
-    })
-  );
-
   const isFreeEvent = event.tickets.length === 0;
   const isPaidEvent = event.tickets.length > 0;
 
   return (
     <>
       <Container className="w-full p-0 m-0">
-        <_SignUpCard numberOfAttendees={attendeeCount?.count ?? 0}>
-          {isFreeEvent && <FreeEventAction event={event} />}
-          {isPaidEvent && (
-            <PaidEventAction
-              event={event}
-              myTicketPromise={myTicketPromise}
-              mePromise={mePromise}
-            />
-          )}
-        </_SignUpCard>
+        <CardBase>
+          <div className="flex items-center gap-x-2">
+            <div className="grow">
+              <Text className="text-gray-400">
+                <Suspense fallback="Loading...">
+                  <UserCount eventId={event.id} />
+                </Suspense>
+              </Text>
+            </div>
+            <div>
+              {isFreeEvent && <FreeEventAction event={event} />}
+              {isPaidEvent && (
+                <PaidEventAction
+                  event={event}
+                  myTicketPromise={myTicketPromise}
+                  mePromise={mePromise}
+                />
+              )}
+            </div>
+          </div>
+        </CardBase>
       </Container>
     </>
   );
+}
+
+function UserCount({eventId}: {eventId: string}) {
+  const attendeeCount = use(
+    api.event.getNumberOfAttendees.query({
+      eventId,
+    })
+  );
+
+  return `${attendeeCount?.count ?? 0} attending`;
 }
--- packages/cards/index.ts
+++ packages/cards/index.ts
@@ -1,10 +1,9 @@
-"use client";
+'use client';
 
-export { DateCard } from "./src/DateCard/DateCard";
-export { EventCard } from "./src/EventCard/EventCard";
-export { InfoCard } from "./src/InfoCard/InfoCard";
-export { OrganizerCard } from "./src/OrganizerCard/OrganizerCard";
-export { LoadingEventCard } from "./src/LoadingEventCard/LoadingEventCard";
-export { LocationCard } from "./src/LocationCard/LocationCard";
-export { SignUpCard } from "./src/SignUpCard/SignUpCard";
-export { UrlCard } from "./src/UrlCard/UrlCard";
+export {DateCard} from './src/DateCard/DateCard';
+export {EventCard} from './src/EventCard/EventCard';
+export {InfoCard} from './src/InfoCard/InfoCard';
+export {OrganizerCard} from './src/OrganizerCard/OrganizerCard';
+export {LoadingEventCard} from './src/LoadingEventCard/LoadingEventCard';
+export {LocationCard} from './src/LocationCard/LocationCard';
+export {UrlCard} from './src/UrlCard/UrlCard';
