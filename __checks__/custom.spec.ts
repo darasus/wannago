0ca1e5ae466a5326d30d24e73df1bcd7fb91319c
Ilya Daraseliya <idarase@gmail.com>
2023-07-09 22:08:26 +0200

    fix

--- __checks__/custom.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test.use({actionTimeout: 10000});
-
-test('Custom Browser Check', async ({page}) => {
-  const response = await page.goto(process.env.ENVIRONMENT_URL!);
-  expect(response?.status()).toBeLessThan(400);
-  await page.screenshot({path: 'screenshot.jpg'});
-});
