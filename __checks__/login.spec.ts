50aa1f4364bffb21b1054c3bc1d92250e65ea7ef
Ilya Daraseliya <idarase@gmail.com>
2023-07-10 07:56:44 +0200

    update timeout

--- __checks__/login.spec.ts
+++ __checks__/login.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 import {testUrl, user_1_email} from './constants';
 
-test.use({actionTimeout: 10000});
+test.use({actionTimeout: 20000});
 
 test('can login', async ({page}) => {
   await page.goto(process.env.ENVIRONMENT_URL || testUrl);
@@ -11,6 +11,6 @@ test('can login', async ({page}) => {
   await page.locator('[data-testid="login-code-input"]').type('424242');
 
   await expect(page.locator('[data-testid="header-user-button"]')).toBeVisible({
-    timeout: 10000,
+    timeout: 20000,
   });
 });
