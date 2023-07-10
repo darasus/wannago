79d49e9876745e2c470ff3f7dc86edf5c45c836c
Ilya Daraseliya <idarase@gmail.com>
2023-07-10 09:38:51 +0200

    fix cors api base

--- packages/utils/src/maps.ts
+++ packages/utils/src/maps.ts
@@ -2,22 +2,21 @@ import type {
   GeocodeResponse,
   PlaceAutocompleteResponse,
 } from '@googlemaps/google-maps-services-js';
-import {getBaseUrl} from './getBaseUrl';
 
 export async function geocode(address: string) {
-  const response = await fetch(`${getBaseUrl()}/api/maps/decode`, {
+  const response = await fetch(`/api/maps/decode`, {
     method: 'POST',
     body: JSON.stringify({address}),
-  }).then(res => res.json());
+  }).then((res) => res.json());
 
   return response as GeocodeResponse['data'];
 }
 
 export async function placeAutocomplete(query: string) {
-  const response = await fetch(`${getBaseUrl()}/api/maps/placeAutocomplete`, {
+  const response = await fetch(`/api/maps/placeAutocomplete`, {
     method: 'POST',
     body: JSON.stringify({query}),
-  }).then(res => res.json());
+  }).then((res) => res.json());
 
   return response as PlaceAutocompleteResponse['data'];
 }
