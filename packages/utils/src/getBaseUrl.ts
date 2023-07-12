590fc34636a9b8e75e01afc3cd3d61a9819c9dd2
Ilya Daraseliya <idarase@gmail.com>
2023-07-12 08:01:17 +0200

    use window.location.origin on the client

--- packages/utils/src/getBaseUrl.ts
+++ packages/utils/src/getBaseUrl.ts
@@ -1,4 +1,8 @@
 export const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    return window.location.origin;
+  }
+
   if (process.env.IS_CHECKLY === 'true' && process.env.ENVIRONMENT_URL) {
     return process.env.ENVIRONMENT_URL;
   }
