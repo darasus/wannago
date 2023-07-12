66ea37ba84a611d304ae832e876e068d777a746f
Ilya Daraseliya <idarase@gmail.com>
2023-07-12 07:49:40 +0200

    allow cors

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -107,6 +107,26 @@ const moduleExports = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {key: 'Access-Control-Allow-Credentials', value: 'true'},
+          {key: 'Access-Control-Allow-Origin', value: '*'},
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
