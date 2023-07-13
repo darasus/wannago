a7478a3e7c0f95cd72b666065af125fb30ef87fa
Ilya Daraseliya <idarase@gmail.com>
2023-07-13 12:01:29 +0200

    save

--- __checks__/utils/getBaseUrl.ts
+++ __checks__/utils/getBaseUrl.ts
@@ -1,1 +1,5 @@
 export {getBaseUrl} from '../../packages/utils/src/getBaseUrl';
+
+// export function getBaseUrl() {
+//   return 'https://wannago-git-enable-kysely-planetscale-driver-darasus-team.vercel.app';
+// }
