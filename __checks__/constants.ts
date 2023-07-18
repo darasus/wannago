ec5c632fdfeead3ebb8729d2b71912f54a8a48f6
Ilya Daraseliya <idarase@gmail.com>
2023-07-18 12:14:47 +0200

    fix test ids

--- __checks__/constants.ts
+++ __checks__/constants.ts
@@ -3,11 +3,11 @@ export const user_1_id = '52cb62d5-c6ca-4da3-be41-d1c3a83dfa21';
 
 export const organization_1_email =
   'idarase+1+organization+automation+clerk_test@gmail.com';
-export const organization_1_id = '5d78757d-c68b-45fd-8954-0e14cb0b6753';
+export const organization_1_id = '0b9bd5f7-3189-495b-89b7-5cfb69be1233';
 
 export const user_2_email = 'idarase+2+automation+clerk_test@gmail.com';
 export const user_2_id = 'ba2c68ad-f288-43c5-b3c5-6bfcc3611a98';
 
 export const organization_2_email =
   'idarase+2+organization+automation+clerk_test@gmail.com';
-export const organization_2_id = '28acab1f-7c9d-4782-a168-393ff902aba5';
+export const organization_2_id = '78dc7b48-2437-4670-85d2-d6698d03e22d';
