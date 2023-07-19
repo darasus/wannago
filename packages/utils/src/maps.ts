588d75270a7858416928ae70934b91f143c36c57
Ilya Daraseliya <idarase@gmail.com>
2023-07-19 22:00:34 +0200

    fix maps utils

--- packages/utils/src/maps.ts
+++ packages/utils/src/maps.ts
@@ -3,21 +3,29 @@ import type {
   PlaceAutocompleteResponse,
 } from '@googlemaps/google-maps-services-js';
 import {getBaseUrl} from './getBaseUrl';
+import axios from 'axios';
+import {captureException} from '@sentry/nextjs';
 
 export async function geocode(address: string) {
-  const response = await fetch(`${getBaseUrl()}/api/maps/decode`, {
-    method: 'POST',
-    body: JSON.stringify({address}),
-  }).then((res) => res.json());
+  const response = await axios
+    .post(`${getBaseUrl()}/api/maps/decode`, {address})
+    .then((res) => res.data)
+    .catch((error) => {
+      captureException(error);
+      return null;
+    });
 
   return response as GeocodeResponse['data'];
 }
 
 export async function placeAutocomplete(query: string) {
-  const response = await fetch(`${getBaseUrl()}/api/maps/placeAutocomplete`, {
-    method: 'POST',
-    body: JSON.stringify({query}),
-  }).then((res) => res.json());
+  const response = await axios
+    .post(`${getBaseUrl()}/api/maps/placeAutocomplete`, {query})
+    .then((res) => res.data)
+    .catch((error) => {
+      captureException(error);
+      return null;
+    });
 
   return response as PlaceAutocompleteResponse['data'];
 }
