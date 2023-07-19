de265ba31864c8e4e88ae9330867cee3fa411b15
Ilya Daraseliya <idarase@gmail.com>
2023-07-19 20:57:29 +0200

    fix maps fetch

--- packages/utils/src/maps.ts
+++ packages/utils/src/maps.ts
@@ -7,7 +7,6 @@ import {getBaseUrl} from './getBaseUrl';
 export async function geocode(address: string) {
   const response = await fetch(`${getBaseUrl()}/api/maps/decode`, {
     method: 'POST',
-    mode: 'no-cors',
     body: JSON.stringify({address}),
   }).then((res) => res.json());
 
@@ -17,7 +16,6 @@ export async function geocode(address: string) {
 export async function placeAutocomplete(query: string) {
   const response = await fetch(`${getBaseUrl()}/api/maps/placeAutocomplete`, {
     method: 'POST',
-    mode: 'no-cors',
     body: JSON.stringify({query}),
   }).then((res) => res.json());
 
