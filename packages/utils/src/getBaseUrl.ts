ee5a52278db0a4344fed48058a32023d36c85d8d
Ilya Daraseliya <idarase@gmail.com>
2023-07-20 08:46:19 +0200

    new get base url

--- packages/utils/src/getBaseUrl.ts
+++ packages/utils/src/getBaseUrl.ts
@@ -1,25 +1,6 @@
 export const getBaseUrl = () => {
-  if (typeof window !== 'undefined') {
-    return window.location.origin;
-  }
-
-  if (process.env.IS_CHECKLY === 'true' && process.env.ENVIRONMENT_URL) {
-    return process.env.ENVIRONMENT_URL;
-  }
-
-  if (process.env.IS_CHECKLY === 'true') {
-    return 'https://wannago-git-test-automation-darasus-team.vercel.app';
-  }
-
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-    return `https://www.wannago.app`;
-  }
-
-  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'development') {
-    return `http://localhost:3000`;
-  }
-
-  const url = process.env.NEXT_PUBLIC_VERCEL_URL;
-
-  return url?.startsWith('http') ? url : `https://${url}`;
+  if (typeof window !== 'undefined') return '';
+  const vc = process.env.VERCEL_URL;
+  if (vc) return `https://${vc}`;
+  return `http://localhost:3000`;
 };
