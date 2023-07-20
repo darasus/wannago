c07be8ac95ffff57bcb459c77e8fdf3a070662f6
Ilya Daraseliya <idarase@gmail.com>
2023-07-20 10:30:15 +0200

    fix get base url

--- packages/utils/src/getBaseUrl.ts
+++ packages/utils/src/getBaseUrl.ts
@@ -1,6 +1,13 @@
 export const getBaseUrl = () => {
   if (typeof window !== 'undefined') return '';
+
   const vc = process.env.VERCEL_URL;
+
+  if (process.env.VERCEL_BRANCH_URL?.includes('test-automation')) {
+    return process.env.VERCEL_BRANCH_URL;
+  }
+
   if (vc) return `https://${vc}`;
+
   return `http://localhost:3000`;
 };
