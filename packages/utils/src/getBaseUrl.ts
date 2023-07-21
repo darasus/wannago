3417d1882e7e9116ce6ae07f9bab9395a92b5e54
Ilya Daraseliya <idarase@gmail.com>
2023-07-21 19:33:22 +0200

    fix base url

--- packages/utils/src/getBaseUrl.ts
+++ packages/utils/src/getBaseUrl.ts
@@ -11,6 +11,10 @@ export const getBaseUrl = () => {
     return 'http://localhost:3000';
   }
 
+  if (process.env.NEXT_PUBLIC_PREVIEW_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_PREVIEW_VERCEL_URL}`;
+  }
+
   const url = process.env.NEXT_PUBLIC_VERCEL_URL;
 
   return url?.startsWith('http') ? url : `https://${url}`;
