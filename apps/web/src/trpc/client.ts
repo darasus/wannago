eea8fe41e9253c802992406447f677baf8a4c930
Ilya Daraseliya <idarase@gmail.com>
2023-07-22 21:05:35 +0200

    disable logger in dev

--- apps/web/src/trpc/client.ts
+++ apps/web/src/trpc/client.ts
@@ -1,11 +1,6 @@
 'use client';
 
-import {loggerLink} from '@trpc/client';
-import {
-  experimental_createActionHook,
-  experimental_createTRPCNextAppDirClient,
-  experimental_serverActionLink,
-} from '@trpc/next/app-dir/client';
+import {experimental_createTRPCNextAppDirClient} from '@trpc/next/app-dir/client';
 import {AppRouter} from 'api';
 import superjson from 'superjson';
 import {endingLink} from './shared';
@@ -15,18 +10,13 @@ export const api = experimental_createTRPCNextAppDirClient<AppRouter>({
     return {
       transformer: superjson,
       links: [
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === 'development' ||
-            (opts.direction === 'down' && opts.result instanceof Error),
-        }),
+        // loggerLink({
+        //   enabled: (opts) =>
+        //     process.env.NODE_ENV === 'development' ||
+        //     (opts.direction === 'down' && opts.result instanceof Error),
+        // }),
         endingLink(),
       ],
     };
   },
-});
-
-export const useAction = experimental_createActionHook({
-  links: [loggerLink(), experimental_serverActionLink()],
-  transformer: superjson,
 });
