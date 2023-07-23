9eb71f27dc55a904a9867730fc20669287bc1bed
Ilya Daraseliya <idarase@gmail.com>
2023-07-23 19:13:36 +0200

    message attendees and add member modal

--- apps/web/src/app/e/[id]/[...page]/features/EventAttendees/features/MessageParticipantsButton/MessageParticipantsButton.tsx
+++ apps/web/src/app/e/[id]/[...page]/features/EventAttendees/features/MessageParticipantsButton/MessageParticipantsButton.tsx
@@ -3,11 +3,20 @@
 import {useState} from 'react';
 import {useForm} from 'react-hook-form';
 import {toast} from 'react-hot-toast';
-import {Button} from 'ui';
-import {MessageParticipantsFormModal} from './components/MessageParticipantsFormModal/MessageParticipantsFormModal';
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  Form,
+  Input,
+  Textarea,
+} from 'ui';
 import {useAmplitude} from 'hooks';
 import {useParams} from 'next/navigation';
 import {api} from '../../../../../../../../trpc/client';
+import {DialogTrigger} from '@radix-ui/react-dialog';
 
 interface Form {
   subject: string;
@@ -19,48 +28,88 @@ export function MessageParticipantsButton() {
   const eventShortId = params?.id as string;
   const {logEvent} = useAmplitude();
   const [isOpen, setIsOpen] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    formState: {isSubmitting},
-    reset,
-  } = useForm<Form>();
+  const form = useForm<Form>();
 
   const handleOnClick = () => {
     setIsOpen(true);
   };
 
-  const handleOnSubmit = handleSubmit(async (data) => {
+  const handleOnSubmit = form.handleSubmit(async (data) => {
     if (eventShortId) {
-      await api.mail.messageEventParticipants.mutate({eventShortId, ...data});
-      toast.success('Message is sent!', {
-        duration: 10000,
-      });
-      setIsOpen(false);
-      reset();
-      logEvent('event_message_to_attendees_submitted', {
-        eventId: eventShortId,
-      });
+      await api.mail.messageEventParticipants
+        .mutate({eventShortId, ...data})
+        .then(() => {
+          toast.success('Message is sent!', {
+            duration: 10000,
+          });
+          setIsOpen(false);
+          form.reset();
+          logEvent('event_message_to_attendees_submitted', {
+            eventId: eventShortId,
+          });
+        })
+        .catch((error) => {
+          toast.error(error.message);
+        });
     }
   });
 
   return (
-    <>
-      <MessageParticipantsFormModal
-        isOpen={isOpen}
-        isSubmitting={isSubmitting}
-        onClose={() => setIsOpen(false)}
-        onSubmit={handleOnSubmit}
-        register={register}
-      />
-      <Button
-        onClick={handleOnClick}
-        size="sm"
-        title="Message attendees"
-        data-testid="message-attendees-button"
-      >
-        Message attendees
-      </Button>
-    </>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button
+          onClick={handleOnClick}
+          size="sm"
+          title="Message attendees"
+          data-testid="message-attendees-button"
+        >
+          Message attendees
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{'Get in touch with participants'}</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={handleOnSubmit}>
+            <div className="flex flex-col gap-y-4">
+              <div>
+                <Input
+                  {...form.register('subject')}
+                  placeholder="Subject"
+                  data-testid="message-attendees-input-subject"
+                />
+              </div>
+              <div>
+                <Textarea
+                  {...form.register('message')}
+                  placeholder="Message"
+                  data-testid="message-attendees-input-message"
+                />
+              </div>
+              <div className="flex justify-start gap-x-2">
+                <Button
+                  disabled={form.formState.isSubmitting}
+                  isLoading={form.formState.isSubmitting}
+                  type="submit"
+                  data-testid="message-attendees-form-submit"
+                >
+                  Send
+                </Button>
+                <Button
+                  onClick={() => {
+                    setIsOpen(false);
+                    form.reset();
+                  }}
+                  variant="outline"
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
--- apps/web/src/app/organizations/[id]/settings/features/TeamMemberSettings/TeamMembersSettings.tsx
+++ apps/web/src/app/organizations/[id]/settings/features/TeamMemberSettings/TeamMembersSettings.tsx
@@ -1,11 +1,32 @@
 'use client';
 
 import {use, useState} from 'react';
-import {Button, CardBase} from 'ui';
-import {CreateMemberModal} from '../CreateMemberModal/CreateMemberModal';
+import {
+  Button,
+  CardBase,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Input,
+} from 'ui';
 import {TeamMember} from '../TeamMember/TeamMember';
 import {Organization} from '@prisma/client';
 import {RouterOutputs} from 'api';
+import {useForm} from 'react-hook-form';
+import {z} from 'zod';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {api} from '../../../../../../trpc/client';
+import {useRouter} from 'next/navigation';
+import {toast} from 'react-hot-toast';
 
 interface Props {
   organization: Organization;
@@ -14,30 +35,94 @@ interface Props {
   >;
 }
 
+const formScheme = z.object({
+  email: z.string().email(),
+});
+
 export function TeamMembersSettings({organization, membersPromise}: Props) {
-  const [isAddMemberDialogModalOpen, setIsAddMemberDialogModalOpen] =
-    useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
   const members = use(membersPromise);
+
+  const form = useForm({
+    resolver: zodResolver(formScheme),
+    defaultValues: {
+      email: '',
+    },
+  });
+
+  const handleSubmit = form.handleSubmit(async (data) => {
+    await api.organization.addOrganizationMember
+      .mutate({
+        userEmail: data.email,
+        organizationId: organization.id,
+      })
+      .then(() => {
+        router.refresh();
+        form.reset();
+        setOpen(false);
+      })
+      .catch((error: any) => {
+        toast.error(error.message);
+      });
+  });
 
   return (
     <>
-      <CreateMemberModal
-        organization={organization}
-        isOpen={isAddMemberDialogModalOpen}
-        onClose={() => setIsAddMemberDialogModalOpen(false)}
-      />
       <div className="flex flex-col gap-4">
         <CardBase
           title={'Organization members'}
           titleChildren={
-            <Button
-              size="sm"
-              variant="link"
-              onClick={() => setIsAddMemberDialogModalOpen(true)}
-              className="p-0"
-            >
-              Add member
-            </Button>
+            <Dialog open={open} onOpenChange={setOpen}>
+              <DialogTrigger asChild>
+                <Button
+                  size="sm"
+                  variant="link"
+                  onClick={() => setOpen(true)}
+                  className="p-0"
+                >
+                  Add member
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Add member</DialogTitle>
+                </DialogHeader>
+                <Form {...form}>
+                  <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col gap-2">
+                      <FormField
+                        name="email"
+                        control={form.control}
+                        render={({field}) => (
+                          <FormItem>
+                            <FormLabel>Member email</FormLabel>
+                            <FormControl>
+                              <Input
+                                {...field}
+                                placeholder="Type email here..."
+                              />
+                            </FormControl>
+                            <FormDescription>
+                              Member account must be created first before adding
+                              to the organization
+                            </FormDescription>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Button
+                        type="submit"
+                        disabled={form.formState.isSubmitting}
+                        isLoading={form.formState.isSubmitting}
+                      >
+                        Add
+                      </Button>
+                    </div>
+                  </form>
+                </Form>
+              </DialogContent>
+            </Dialog>
           }
         >
           <div className="flex flex-col gap-y-2">
