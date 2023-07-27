acc8aea9d233631e8368eb5088f58dde012c5335
Ilya Daraseliya <idarase@gmail.com>
2023-07-27 10:28:47 +0200

    disable sentry tracing

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -4,6 +4,4 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  tracesSampleRate: process.env.NODE_ENV === 'development' ? 1 : 0.1,
-  debug: false,
 });
