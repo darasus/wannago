34760e8ce5997b6b80969d4ca5174d3482e96adf
Ilya Daraseliya <idarase@gmail.com>
2023-08-08 21:54:59 +0200

    fixes

--- apps/web/src/components/Marketing/Blob.tsx
+++ apps/web/src/components/Marketing/Blob.tsx
@@ -2,19 +2,25 @@
 
 import {useEffect, useRef} from 'react';
 import {Application} from '@splinetool/runtime';
+import {useTheme} from 'next-themes';
 
 export default function Blob() {
   const ref = useRef<HTMLCanvasElement>(null);
+  const {resolvedTheme} = useTheme();
 
   useEffect(() => {
     if (typeof window !== 'undefined' && ref.current) {
       const app = new Application(ref.current);
-      app.load('https://prod.spline.design/uIkYeCM5LMKNkXlz/scene.splinecode');
+      app.load(
+        resolvedTheme === 'dark'
+          ? 'https://prod.spline.design/uIkYeCM5LMKNkXlz/scene.splinecode'
+          : 'https://prod.spline.design/fSgRvzLa1vBCVtFp/scene.splinecode'
+      );
     }
-  }, []);
+  }, [resolvedTheme]);
 
   return (
-    <div className="block h-[1000px] w-full overflow-hidden pointer-events-none border-b-2 border-primary">
+    <div className="block h-[1000px] w-full overflow-hidden pointer-events-none border-b">
       <canvas ref={ref} id="canvas3d" className="block h-full w-full" />
     </div>
   );
