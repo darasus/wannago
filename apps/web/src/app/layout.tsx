67d9d138a44636747de810595be9e5fa1d79838d
Ilya Daraseliya <idarase@gmail.com>
2023-08-22 19:58:30 +0200

    fix

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
               <Header />
             </Container>
           </div>
+          <div>
+            <div>{children}</div>
+          </div>
           <Tools />
           <ToastProvider />
         </ClientProvider>
