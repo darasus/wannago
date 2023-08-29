13014cd57ffc213dadb6959668f00d8ebe048464
Ilya Daraseliya <idarase@gmail.com>
2023-08-29 17:01:34 +0200

    fix prisma instance duplicates

--- packages/database/prisma.ts
+++ packages/database/prisma.ts
@@ -1,84 +1,35 @@
-// import {PrismaClient} from '@prisma/client/edge';
-// import {getCurrentHub} from '@sentry/nextjs';
-// import {withAccelerate} from '@prisma/extension-accelerate';
-
-// let prisma: PrismaClient;
-
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!(global as any).prisma) {
-//     (global as any).prisma = new PrismaClient({
-//       // log:
-//       //   process.env.NODE_ENV === 'development'
-//       //     ? ['query', 'error', 'warn']
-//       //     : ['error'],
-//     });
-//   }
-//   prisma = (global as any).prisma;
-// }
-
-// prisma.$use(async (params, next) => {
-//   const before = Date.now();
-
-//   const result = await next(params);
-
-//   const after = Date.now();
-
-//   if (process.env.NODE_ENV === 'development') {
-//     console.log(
-//       `[PRISMA_QUERY] ${params.model}.${params.action} took ${after - before}ms`
-//     );
-//   }
-
-//   return result;
-// });
-
-// prisma.$use(async (params, next) => {
-//   const {model, action, runInTransaction, args} = params;
-//   const description = [model, action].filter(Boolean).join('.');
-//   const data = {
-//     model,
-//     action,
-//     runInTransaction,
-//     args,
-//   };
-
-//   const scope = getCurrentHub().getScope();
-//   const parentSpan = scope?.getSpan();
-//   const span = parentSpan?.startChild({
-//     op: 'db',
-//     description,
-//     data,
-//   });
-
-//   scope?.addBreadcrumb({
-//     category: 'db',
-//     message: description,
-//     data,
-//   });
-
-//   const result = await next(params);
-//   span?.finish();
-
-//   return result;
-// });
-
-// prisma.$extends(withAccelerate());
-
-// export {prisma};
-
 import {PrismaClient} from '@prisma/client';
 
-function makePrisma() {
-  return new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient({
+      // log:
+      //   process.env.NODE_ENV === 'development'
+      //     ? ['query', 'error', 'warn']
+      //     : ['error'],
+    });
+  }
+  prisma = (global as any).prisma;
 }
 
-const globalForPrisma = global as unknown as {
-  prisma: ReturnType<typeof makePrisma>;
-};
+prisma.$use(async (params, next) => {
+  const before = Date.now();
 
-export const prisma = globalForPrisma.prisma ?? makePrisma();
+  const result = await next(params);
 
-if (process.env.NODE_ENV !== 'production')
-  globalForPrisma.prisma = makePrisma();
+  const after = Date.now();
+
+  if (process.env.NODE_ENV === 'development') {
+    console.log(
+      `[PRISMA_QUERY] ${params.model}.${params.action} took ${after - before}ms`
+    );
+  }
+
+  return result;
+});
+
+export {prisma};
