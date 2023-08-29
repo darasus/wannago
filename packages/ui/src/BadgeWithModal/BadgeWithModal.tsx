a99cbd640f8cebc8359b7342ba14eb44ba505485
Ilya Daraseliya <idarase@gmail.com>
2023-08-29 08:39:28 +0200

    fix info card badge

--- packages/ui/src/BadgeWithModal/BadgeWithModal.tsx
+++ packages/ui/src/BadgeWithModal/BadgeWithModal.tsx
@@ -1,4 +1,3 @@
-import {Badge} from '../Badge/Badge';
 import {cn} from 'utils';
 import {
   Dialog,
@@ -7,6 +6,7 @@ import {
   DialogTrigger,
 } from '../Dialog/Dialog';
 import {ReactNode} from 'react';
+import {ColoredBadge} from '../Badge/ColoredBadge';
 
 interface Props {
   className?: string;
@@ -20,20 +20,9 @@ export function BadgeWithModal({className, text, children, color}: Props) {
     <Dialog>
       <DialogTrigger>
         <div className={cn('inline-flex bg-background rounded-lg', className)}>
-          <Badge
-            className={cn('border text-primary', {
-              'bg-green-400/20 border-green-600/20 border-green-600/20':
-                color === 'green',
-              'bg-red-400/20 hover:bg-red-600/20 border-red-600/20':
-                color === 'red',
-              'bg-blue-400/20 hover:bg-blue-600/20 border-blue-600/20':
-                color === 'blue',
-              'bg-yellow-400/20 hover:bg-yellow-600/20 border-yellow-600/20':
-                color === 'yellow',
-            })}
-          >
+          <ColoredBadge className="py-1" color={color}>
             {children}
-          </Badge>
+          </ColoredBadge>
         </div>
       </DialogTrigger>
       <DialogContent>
