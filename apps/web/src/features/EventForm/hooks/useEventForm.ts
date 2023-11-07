be19ec6476f951734075af477f278e71a5fb4070
Ilya Daraseliya <idarase@gmail.com>
2023-11-07 22:21:09 +0100

    add validation

--- apps/web/src/features/EventForm/hooks/useEventForm.ts
+++ apps/web/src/features/EventForm/hooks/useEventForm.ts
@@ -15,53 +15,70 @@ import {zodResolver} from '@hookform/resolvers/zod';
 import {parseISO} from 'date-fns';
 import {generateEventCode} from 'utils/src/generateShortId';
 
-export const eventFormSchema = z.object({
-  createdById: z.string().min(1),
-  title: z
-    .string({
-      required_error: 'Title is required',
-    })
-    .min(1, 'Title is required'),
-  description: z.string().optional(),
-  featuredImageSrc: z.string().nullable().optional(),
-  featuredImageHeight: z.number().nullable().optional(),
-  featuredImageWidth: z.number().nullable().optional(),
-  featuredImagePreviewSrc: z.string().nullable().optional(),
-  startDate: z.date({
-    required_error: 'Start date is required.',
-  }),
-  endDate: z.date({
-    required_error: 'End date is required.',
-  }),
-  address: z
-    .string({
-      required_error: 'Address is required',
-    })
-    .min(1, 'Address is required'),
-  maxNumberOfAttendees: z.string().optional().default('0'),
-  tickets: z
-    .array(
-      z.object({
-        id: z.string().optional(),
-        title: z.string(),
-        description: z.string().optional(),
-        price: z.string(),
-        maxQuantity: z.string(),
+export const eventFormSchema = z
+  .object({
+    createdById: z.string().min(1),
+    title: z
+      .string({
+        required_error: 'Title is required',
       })
-    )
-    .optional(),
-  currency: z.nativeEnum(Currency),
-  eventVisibility: z.nativeEnum(EventVisibility).optional(),
-  signUpProtection: z.nativeEnum(SignUpProtection).optional(),
-  eventVisibilityCode: z
-    .string()
-    .max(20, 'Code must be less than 20 characters')
-    .optional(),
-  signUpProtectionCode: z
-    .string()
-    .max(20, 'Code must be less than 20 characters')
-    .optional(),
-});
+      .min(1, 'Title is required'),
+    description: z.string().optional(),
+    featuredImageSrc: z.string().nullable().optional(),
+    featuredImageHeight: z.number().nullable().optional(),
+    featuredImageWidth: z.number().nullable().optional(),
+    featuredImagePreviewSrc: z.string().nullable().optional(),
+    startDate: z.date({
+      required_error: 'Start date is required.',
+    }),
+    endDate: z.date({
+      required_error: 'End date is required.',
+    }),
+    address: z
+      .string({
+        required_error: 'Address is required',
+      })
+      .min(1, 'Address is required'),
+    maxNumberOfAttendees: z.string().optional().default('0'),
+    tickets: z
+      .array(
+        z.object({
+          id: z.string().optional(),
+          title: z.string(),
+          description: z.string().optional(),
+          price: z.string(),
+          maxQuantity: z.string(),
+        })
+      )
+      .optional(),
+    currency: z.nativeEnum(Currency),
+    eventVisibility: z.nativeEnum(EventVisibility).optional(),
+    signUpProtection: z.nativeEnum(SignUpProtection).optional(),
+    eventVisibilityCode: z
+      .string()
+      .max(20, 'Code must be less than 20 characters')
+      .optional(),
+    signUpProtectionCode: z
+      .string()
+      .max(20, 'Code must be less than 20 characters')
+      .optional(),
+  })
+  .superRefine(({signUpProtection, eventVisibility}, ctx) => {
+    if (signUpProtection === 'PROTECTED' && eventVisibility === 'PROTECTED') {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['eventVisibility'],
+        message:
+          "You can't set both private event visibility and sign up protection with code",
+      });
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['signUpProtection'],
+        message:
+          "You can't set both private event visibility and sign up protection with code",
+      });
+    }
+  });
 
 export function useEventForm(props: {
   event?: (Event & {tickets: Ticket[]}) | null;
