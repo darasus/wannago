2cef411e19ddcde5424ef031326de5852ca65e2d
Ilya Daraseliya <idarase@gmail.com>
2023-11-16 11:37:30 +0100

    update dependencies

--- packages/database/prisma.ts
+++ packages/database/prisma.ts
@@ -1,11 +1,11 @@
-import {connect} from '@planetscale/database';
+import {Client} from '@planetscale/database';
 import {PrismaPlanetScale} from '@prisma/adapter-planetscale';
 import {PrismaClient} from '@prisma/client';
 import {fetch as undiciFetch} from 'undici';
 
 const connectionString = `${process.env.DATABASE_URL}`;
-const connection = connect({url: connectionString, fetch: undiciFetch});
-const adapter = new PrismaPlanetScale(connection);
+const client = new Client({url: connectionString, fetch: undiciFetch});
+const adapter = new PrismaPlanetScale(client);
 export const prisma = new PrismaClient({adapter});
 
 // let prisma: PrismaClient;
