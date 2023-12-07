d2cc66460f6336aa2a77f2bc19df6860b77e844b
Ilya Daraseliya <idarase@gmail.com>
2023-12-07 21:44:31 +0100

    clean up

--- packages/api/src/actions/createPaymentIntent.ts
+++ packages/api/src/actions/createPaymentIntent.ts
@@ -24,50 +24,6 @@ export function createPaymentIntent(ctx: ActionContext) {
   }: Props) => {
     invariant(ctx.auth?.user.id, userNotFoundError);
 
-    // const event = await prisma.event.findUnique({
-    //   where: {
-    //     id: eventId,
-    //   },
-    //   include: {
-    //     organization: true,
-    //     user: true,
-    //     tickets: true,
-    //     ticketSales: true,
-    //   },
-    // });
-
-    // invariant(event, eventNotFoundError);
-
-    // const selectedTickets = await prisma.ticket.findMany({
-    //   where: {
-    //     id: {
-    //       in: tickets.map((ticket) => ticket.ticketId),
-    //     },
-    //   },
-    // });
-
-    // const totalAmount = tickets.reduce((acc, ticket) => {
-    //   const {price} =
-    //     selectedTickets.find((t) => t.id === ticket.ticketId) || {};
-
-    //   if (!price) {
-    //     return acc;
-    //   }
-
-    //   return acc + ticket.quantity * price;
-    // }, 0);
-
-    // const successCallbackUrl = `${getBaseUrl()}/e/${
-    //   event.shortId
-    // }/my-tickets?success=true`;
-    // const cancelCallbackUrl = `${getBaseUrl()}/e/${event.shortId}`;
-
-    // const stripeLinkedAccountId =
-    //   event?.organization?.stripeLinkedAccountId ||
-    //   ctx.auth.user.stripeLinkedAccountId;
-
-    // invariant(stripeLinkedAccountId);
-
     const session = await ctx
       .createStripeClient(stripeLinkedAccountId)
       .paymentIntents.create(
@@ -81,39 +37,6 @@ export function createPaymentIntent(ctx: ActionContext) {
             eventId: eventId,
             ticketSaleIds: JSON.stringify(ticketSaleIds),
           },
-          // customer_email: stripeCustomerId ? undefined : email,
-          // customer_update: stripeCustomerId
-          //   ? {
-          //       name: 'auto',
-          //       address: 'auto',
-          //     }
-          //   : undefined,
-          // billing_address_collection: 'auto',
-          // line_items: selectedTickets.map((ticket) => {
-          //   return {
-          //     quantity: tickets.find((t) => t.ticketId === ticket.id)?.quantity,
-          //     price_data: {
-          //       product_data: {
-          //         name: ticket.title,
-          //       },
-          //       currency: event.preferredCurrency || 'USD',
-          //       unit_amount: ticket.price,
-          //     },
-          //   };
-          // }),
-          // mode: 'payment',
-          // success_url: successCallbackUrl,
-          // cancel_url: cancelCallbackUrl,
-          // tax_id_collection: {
-          //   enabled: true,
-          // },
-          // allow_promotion_codes: false,
-          // payment_intent_data: {
-          //   application_fee_amount: calculateFee(totalAmount),
-          //   transfer_data: {
-          //     destination: stripeLinkedAccountId,
-          //   },
-          // },
         },
         {
           stripeAccount: stripeLinkedAccountId,
