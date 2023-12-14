7f416972c88b4af186ed06bdc109fe3d903cb1d7
Ilya Daraseliya <idarase@gmail.com>
2023-12-14 21:03:17 +0100

    add suspense

---
+++ apps/web/src/features/Header/MyEventsButton.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import {Button} from 'ui';
+import {api} from '../../trpc/server-http';
+
+export async function MyEventsButton() {
+  const me = await api.user.me.query();
+
+  if (me) {
+    return null;
+  }
+
+  return (
+    <Button
+      asChild
+      className="hidden md:flex"
+      variant="default"
+      size="sm"
+      data-testid="login-button"
+    >
+      <Link href="/sign-in">Sign in</Link>
+    </Button>
+  );
+}
